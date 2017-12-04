772f810788bdcd6f03cd401a6aecbff17d3fd5d4
yanxuebiao <xbyan@gongsibao.com>
2017-12-04 11:13:01 +0800

    修改编码

--- gsb/gsb-entity/src/main/java/com/gongsibao/entity/cms/Product.java
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/cms/Product.java
@@ -8,88 +8,88 @@ import org.netsharp.core.annotations.Table;
 
 import com.gongsibao.entity.BaseEntity;
 
-@Table(name="cms_product",header="")
+@Table(name="cms_product",header="cms产品表")
 public class Product extends BaseEntity {
     /**   
-	 * @Fields serialVersionUID : TODO(鐢ㄤ竴鍙ヨ瘽鎻忚堪杩欎釜鍙橀噺琛ㄧず浠�涔�)   
+	 * @Fields serialVersionUID : TODO()   
 	 */   
 	private static final long serialVersionUID = -1099819795496453198L;
-	@Column(name="product_id",header="")
+	@Column(name="product_id",header="产品序号")
     private Integer productId;
 	
-    @Column(name="package_id",header="")
+    @Column(name="package_id",header="套餐序号")
     private Integer packageId;
     
     @Column(name="cooperation_company_id",header="合作公司")
     private Integer cooperationCompanyId;
     
-    @Column(name="showprice",header="")
+    @Column(name="showprice",header="显示价")
     private String showprice;
     
-    @Column(name="prod_name",header="")
+    @Column(name="prod_name",header="商品或套餐名称(显示名称)")
     private String prodName;
     
-    @Column(name="summary",header="")
+    @Column(name="summary",header="摘要")
     private String summary;
     
-    @Column(name="app_pord_img_url_id",header="")
+    @Column(name="app_pord_img_url_id",header="手机产品图id")
     private Integer appPordImgUrlId;
     
-    @Column(name="un_app_pord_img_url_id",header="")
+    @Column(name="un_app_pord_img_url_id",header="未选中手机产品图id")
     private Integer unAppPordImgUrlId;
     
-    @Column(name="prod_icon_img_url_id",header="")
+    @Column(name="prod_icon_img_url_id",header="产品小图标id")
     private Integer prodIconImgUrlId;
     
-    @Column(name="package_prod_icon_url_id",header="")
+    @Column(name="package_prod_icon_url_id",header="套餐里面产品的icon小图id")
     private Integer packageProdIconUrlId;
     
-    @Column(name="status",header="")
+    @Column(name="status",header="状态（0：待发布，1：已发布）")
     private Integer status;
     
-    @Column(name="php_id",header="")
+    @Column(name="php_id",header="phpid")
     private Integer phpId;
     
-    @Column(name="sort",header="")
+    @Column(name="sort",header="排序编号")
     private Double sort;
     
-    @Column(name="is_show",header="")
+    @Column(name="is_show",header="是否显示（0：不显示，1：显示）")
     private Integer isShow;
     
-    @Column(name="is_hot",header="")
+    @Column(name="is_hot",header="是否是热门推广（0：不是，1：是）")
     private Integer isHot;
     
-    @Column(name="last_update_time",header="")
+    @Column(name="last_update_time",header="最后修改时间")
     private Date lastUpdateTime;
     
-    @Column(name="attribute_category",header="")
+    @Column(name="attribute_category",header="性类别（0：单品；1：聚合；2：无层级聚合；3：套餐）")
     private Integer attributeCategory;
     
-    @Column(name="first_aggregation_name",header="")
+    @Column(name="first_aggregation_name",header="一级聚合分类名称'")
     private String firstAggregationName;
     
-    @Column(name="second_aggregation_name",header="")
+    @Column(name="second_aggregation_name",header="二级聚合分类名称")
     private String secondAggregationName;
     
-    @Column(name="price_description",header="")
+    @Column(name="price_description",header="价格描述")
     private String priceDescription;
     
-    @Column(name="promotional_copy",header="")
+    @Column(name="promotional_copy",header="一句话卖点(推广文案)")
     private String promotionalCopy;
     
-    @Column(name="service_area_description",header="")
+    @Column(name="service_area_description",header="服务地区文案")
     private String serviceAreaDescription;
     
-    @Column(name="regist_address_description",header="")
+    @Column(name="regist_address_description",header="注册地址文案")
     private String registAddressDescription;
     
-    @Column(name="service_period_description",header="")
+    @Column(name="service_period_description",header="服务期文案")
     private String servicePeriodDescription;
     
-    @Column(name="buy_count_description",header="")
+    @Column(name="buy_count_description",header="购买数量的文案")
     private String buyCountDescription;
 
-    @Column(name="remark",header="")
+    @Column(name="remark",header="备注")
     private String remark;
 
 
