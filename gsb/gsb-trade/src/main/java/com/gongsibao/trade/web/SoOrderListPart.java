2775a33114c47ad32be78864ba136de3a35b913c
yanxuebiao <xbyan@gongsibao.com>
2017-12-06 19:15:16 +0800

    万达的订单详情

---
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/SoOrderListPart.java
@@ -0,0 +1,18 @@
+package com.gongsibao.trade.web;
+
+import java.util.List;
+
+import org.netsharp.communication.ServiceFactory;
+import org.netsharp.panda.commerce.ListPart;
+
+import com.gongsibao.entity.trade.OrderProdTrace;
+import com.gongsibao.trade.base.IOrderProdTraceService;
+
+
+public class SoOrderListPart extends ListPart{
+	IOrderProdTraceService prodTraceService = ServiceFactory.create(IOrderProdTraceService.class);
+	public List<OrderProdTrace> querySoOrderTraceList(Integer soOrderId){
+		List<OrderProdTrace> orderTraceList= prodTraceService.querySoOrderTraceList(soOrderId);
+		return orderTraceList;
+	}
+}
