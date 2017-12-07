c8ac3384ebb07787bda5bd1ac9c2f4f181fe9ae5
王昆 <wangkun@gongsibao.com>
2017-12-07 16:06:26 +0800

    商标名称查询api

---
+++ gsb/gsb-taurus-sdk/src/main/java/com/gongsibao/taurus/service/TmByNameService.java
@@ -0,0 +1,24 @@
+package com.gongsibao.taurus.service;
+
+import com.gongsibao.taurus.api.ApiFactory;
+import com.gongsibao.taurus.api.TmByNameApi;
+import com.gongsibao.taurus.entity.Tm;
+import com.gongsibao.taurus.message.ResponseMessage;
+
+import java.util.List;
+
+/**
+ * Created by wk on 2017/12/7.
+ */
+public class TmByNameService implements ITaurusApiService<Tm> {
+    @Override
+    public List<Tm> get(String name) {
+        TmByNameApi api = ApiFactory.create(TmByNameApi.class);
+        api.setName(name);
+        ResponseMessage<Tm> response = api.getResponse();
+        if (response == null) {
+            return null;
+        }
+        return response.getList();
+    }
+}
