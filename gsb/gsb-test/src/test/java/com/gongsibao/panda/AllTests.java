d58bc4734ab63e82b46972e46911f8beb4f18f9f
yanxuebiao <xbyan@gongsibao.com>
2017-12-07 20:38:43 +0800

    添加日报报表

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/AllTests.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/AllTests.java
@@ -6,27 +6,26 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	
-	org.netsharp.meta.framework.AllTests.class,
-	org.netsharp.meta.platform.AllTests.class,
-	org.netsharp.meta.basebiz.AllTests.class,
-	
-//	org.netsharp.wx.meta.AllTests.class,
-//    org.netsharp.cache.plugin.AllTests.class,
-//    org.netsharp.scrum.meta.AllTests.class,
-//    com.gongsibao.ma.AllTest.class,
-//    com.gongsibao.er.AllTest.class,
-      com.gongsibao.crm.AllTest.class,
-      com.gongsibao.cms.AllTest.class,
-      com.gongsibao.trade.AllTest.class,
 
-      com.gongsibao.taurus.AllTest.class,
-      //com.gongsibao.uc.AllTest.class,
-//    com.gongsibao.franchisee.AllTest.class,
-    
-	org.netsharp.meta.end.AllTests.class,
-	
-	})
+		org.netsharp.meta.framework.AllTests.class,
+		org.netsharp.meta.platform.AllTests.class,
+		org.netsharp.meta.basebiz.AllTests.class,
+
+		// org.netsharp.wx.meta.AllTests.class,
+		// org.netsharp.cache.plugin.AllTests.class,
+		// org.netsharp.scrum.meta.AllTests.class,
+		// com.gongsibao.ma.AllTest.class,
+		// com.gongsibao.er.AllTest.class,
+		com.gongsibao.crm.AllTest.class, 
+		com.gongsibao.cms.AllTest.class,
+		com.gongsibao.trade.AllTest.class,
+
+		com.gongsibao.taurus.AllTest.class,
+		// com.gongsibao.uc.AllTest.class,
+		com.gongsibao.franchisee.AllTest.class,
+		org.netsharp.meta.end.AllTests.class,
+
+})
 public class AllTests {
 
 }
