777bde38d5c26f628648d1c54be146787e116e00
yanxuebiao <xbyan@gongsibao.com>
2017-12-08 10:54:08 +0800

    修改供应商报表

--- gsb/gsb-bd/src/main/java/com/gongsibao/franchisee/base/IFranchiseeService.java
+++ gsb/gsb-bd/src/main/java/com/gongsibao/franchisee/base/IFranchiseeService.java
@@ -1,21 +1,21 @@
 package com.gongsibao.franchisee.base;
 
-import java.util.List;
 import java.util.Map;
 
 import org.netsharp.base.IPersistableService;
 
 import com.gongsibao.entity.franchisee.Franchisee;
-import com.gongsibao.entity.franchisee.dto.FranchiseeReportDto;
+import com.gongsibao.entity.franchisee.FranchiseeReport;
 
 public interface IFranchiseeService extends IPersistableService<Franchisee>{
-
+	
 	/**
 	 * 获取当前时间截止的客户总数量
+	 * @param ownerId 业务员Id
 	 * @param currentTime 当前时间
 	 * @return 业务员Id和对应的总客户 的集合
 	 */
-	public Map<Integer, Integer> getCustomersAllTotal(String currentTime);
+	public Map<Integer, Integer> getCustomersAllTotal(Integer ownerId,String currentTime);
 	
 	/**
 	 * 根据条件查询符合报表数据的集合
@@ -23,5 +23,5 @@ public interface IFranchiseeService extends IPersistableService<Franchisee>{
 	 * @param currentTime 当前时间
 	 * @return
 	 */
-	public FranchiseeReportDto getList(Integer ownerId,String currentTime);
+	public FranchiseeReport getReportEntity(Integer ownerId,String currentTime);
 }
