43cd7a3fa5123439b80426c543d705632f0323e3
hanwei <hwhping@163.com>
2017-12-12 08:48:06 +0800

    测试用例整理

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/user/AllTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/user/AllTest.java
@@ -4,12 +4,31 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.gongsibao.panda.user.reference.OrganizationReferenceTest;
+import com.gongsibao.panda.user.reference.RoleReferenceTest;
+import com.gongsibao.panda.user.reference.UserReferenceTest;
+import com.gongsibao.panda.user.workspace.user.AuthWorkspaceTest;
+import com.gongsibao.panda.user.workspace.user.LoginLogWorkspaceTest;
+import com.gongsibao.panda.user.workspace.user.OrganizationWorkspaceTest;
+import com.gongsibao.panda.user.workspace.user.RoleWorkspaceTest;
+import com.gongsibao.panda.user.workspace.user.UserWorkspaceTest;
+
+
+
 @RunWith(Suite.class)
 @SuiteClasses({
 
 	ResourceTest.class, 
 
-
+	RoleReferenceTest.class,
+	OrganizationReferenceTest.class,
+	UserReferenceTest.class,
+	
+	OrganizationWorkspaceTest.class,
+	UserWorkspaceTest.class,
+	RoleWorkspaceTest.class,
+	AuthWorkspaceTest.class,
+	LoginLogWorkspaceTest.class,
 	
 	NavigationTest.class
 	
