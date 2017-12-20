e5f2544014f80919723121b6c0decacc7ec8ff1c
zhangchao <chaozhang@gongsibao.com>
2017-12-20 18:10:05 +0800

    fix

--- gsb/gsb-bd/src/main/java/com/gongsibao/u8/service/ManualVoucherOrderDTOService.java
+++ gsb/gsb-bd/src/main/java/com/gongsibao/u8/service/ManualVoucherOrderDTOService.java
@@ -3,8 +3,10 @@ package com.gongsibao.u8.service;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.netsharp.communication.Service;
+import org.netsharp.communication.ServiceFactory;
 import org.netsharp.core.DataTable;
 import org.netsharp.core.IRow;
 import org.netsharp.core.Oql;
@@ -15,6 +17,8 @@ import com.gongsibao.entity.trade.dic.OrderIsManualVoucherType;
 import com.gongsibao.entity.trade.dic.OrderManualVoucherStatus;
 import com.gongsibao.entity.trade.dto.ManualVoucherOrderDTO;
 import com.gongsibao.u8.base.IManualVoucherOrderDTOService;
+import com.gongsibao.u8.base.ISoOrderService;
+import com.gongsibao.u8.base.IUserService;
 
 @Service
 public class ManualVoucherOrderDTOService extends PersistableService<ManualVoucherOrderDTO> implements IManualVoucherOrderDTOService {
@@ -33,14 +37,7 @@ public class ManualVoucherOrderDTOService extends PersistableService<ManualVouch
 		int startIndex = (paging.getPageNo() - 1) * paging.getPageSize();
 		StringBuffer sqlBuffer = new StringBuffer();
 		sqlBuffer.append("SELECT * FROM ( ");
-		sqlBuffer.append("SELECT (SELECT uc_user.`real_name` FROM so_order  ");
-		sqlBuffer.append("JOIN (SELECT MIN(so_order_prod.pkid) 'pkid', so_order_prod.order_id FROM so_order_prod GROUP BY order_id) so_order_prod1 ON so_order.`pkid`= so_order_prod1.`order_id`  ");
-		sqlBuffer.append("JOIN so_order_prod_user_map ON so_order_prod_user_map.`order_prod_id`=so_order_prod1.pkid AND `so_order_prod_user_map`.`type_id`=3061 AND `so_order_prod_user_map`.`status_id`=3141  ");
-		sqlBuffer.append("JOIN uc_user ON uc_user.`pkid` = so_order_prod_user_map.`user_id` WHERE so_order.`pkid`= oi.pkid) 'operator', ");
-		sqlBuffer.append("(CASE WHEN (cri1.`pkid` IS NOT NULL AND cri1.`company_name`!='' ) THEN cri1.`company_name`  ");
-		sqlBuffer.append("WHEN (c.pkid IS NULL) THEN (CASE WHEN a.real_name='' THEN a.name ELSE a.real_name END) ");
-		sqlBuffer.append("WHEN (ccm.pkid IS NULL OR cri.company_name='') THEN (CASE WHEN c.real_name='' THEN c.`mobile` ELSE c.real_name END) ELSE cri.company_name END) 'custName', ");
-		sqlBuffer.append("oi.pkid 'id',oi.no 'orderNo',oi.payable_price 'payablePrice',oi.paid_price 'paidPrice', ");
+		sqlBuffer.append("SELECT oi.pkid 'id',oi.no 'orderNo',oi.payable_price 'payablePrice',oi.paid_price 'paidPrice', ");
 		sqlBuffer.append("oi.manual_voucher_status 'manualVoucherStatus',oi.is_manual_voucher 'isManualVoucher', ");
 		sqlBuffer.append("oi.pay_time 'returnTime',  ");
 		sqlBuffer.append("oi.add_time 'addTime' ");
@@ -58,9 +55,11 @@ public class ManualVoucherOrderDTOService extends PersistableService<ManualVouch
 		oql.setPaging(paging);
 		DataTable dataTable = this.pm.executeTable(sqlBuffer.toString(), null);
 		List<ManualVoucherOrderDTO> reslis = new ArrayList<>();
+
+		List<Integer> orderIdList = new ArrayList();
+
 		for (IRow row : dataTable) {
 			ManualVoucherOrderDTO dto = new ManualVoucherOrderDTO();
-
 			Integer payablePrice = row.getInteger("payablePrice");// 订单应付价
 			Integer paidPrice = row.getInteger("paidPrice");// 订单已付价
 			dto.setId(row.getInteger("id"));
@@ -73,8 +72,22 @@ public class ManualVoucherOrderDTOService extends PersistableService<ManualVouch
 			dto.setPayablePrice(payablePrice == null ? 0 : getDivRes(payablePrice, 100));
 			dto.setAddTime(row.getDate("addTime"));
 			dto.setReturnTime(row.getDate("returnTime"));
-
+			orderIdList.add(dto.getId());
 			reslis.add(dto);
+		}
+
+		
+		IUserService userService = ServiceFactory.create(IUserService.class);
+		//根据订单id集合获取订单操作员
+		Map<Integer, String> operatorByOrderIdMap = userService.getOperatorByOrderIds(orderIdList);
+
+		ISoOrderService soOrderService = ServiceFactory.create(ISoOrderService.class);
+		//根据订单id集合获取订单客户名称
+		Map<Integer, String> custNameByOrderIdList = soOrderService.getCustNameByOrderIdList(orderIdList);
+
+		for (ManualVoucherOrderDTO dto : reslis) {
+			dto.setOperator(operatorByOrderIdMap.get(dto.getId()));
+			dto.setCustName(custNameByOrderIdList.get(dto.getId()));
 		}
 
 		return reslis;
