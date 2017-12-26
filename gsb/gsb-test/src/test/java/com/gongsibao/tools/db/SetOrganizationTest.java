331d6f01a286bd1a6d1d2e6914a5c70f9b584d14
yanxuebiao <xbyan@gongsibao.com>
2017-12-26 16:55:03 +0800

    修改组织机构

---
+++ gsb/gsb-test/src/test/java/com/gongsibao/tools/db/SetOrganizationTest.java
@@ -0,0 +1,41 @@
+package com.gongsibao.tools.db;
+
+import org.junit.Test;
+import org.netsharp.communication.ServiceFactory;
+import org.netsharp.core.DataTable;
+import org.netsharp.core.IRow;
+import org.netsharp.organization.entity.Employee;
+import org.netsharp.persistence.IPersister;
+import org.netsharp.persistence.PersisterFactory;
+
+import com.gongsibao.uc.base.IOrganizationService;
+
+public class SetOrganizationTest {
+	IOrganizationService organizationService = ServiceFactory.create(IOrganizationService.class);
+	IPersister<Employee> pm = PersisterFactory.create();
+	@Test
+	public void run() {
+		doRun();
+	}
+	public void doRun() {
+		String getParentId ="SELECT pkid from uc_organization where pid=0";
+		DataTable getDt = organizationService.executeTable(getParentId.toString(), null);
+		for (IRow row : getDt) {
+			Integer getPkid = Integer.parseInt(row.getString("pkid"));
+			setOrfanization(getPkid);
+		}
+	}
+	private void setOrfanization(Integer pid) {
+		String getParentId ="SELECT pkid from uc_organization where pid="+pid;
+		DataTable getDt = organizationService.executeTable(getParentId.toString(), null);
+		if(getDt.size()>0){
+			for (IRow row : getDt) {
+				setOrfanization(Integer.parseInt(row.getString("pkid")));  
+	        }  	
+		}else{
+			String updateSql = "UPDATE uc_organization set is_leaf=1 where pkid="+pid;
+			int i = pm.executeNonQuery(updateSql.toString(), null);
+		}
+		
+    }  
+}
