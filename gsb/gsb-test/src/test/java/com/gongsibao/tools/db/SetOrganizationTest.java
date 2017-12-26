3306b6e6fa0f7eaa6756463a54bfc7fad450f1a8
hanwei <hwhping@163.com>
2017-12-26 17:17:30 +0800

    增加作业启动

--- gsb/gsb-test/src/test/java/com/gongsibao/tools/db/SetOrganizationTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/tools/db/SetOrganizationTest.java
@@ -13,29 +13,32 @@ import com.gongsibao.uc.base.IOrganizationService;
 public class SetOrganizationTest {
 	IOrganizationService organizationService = ServiceFactory.create(IOrganizationService.class);
 	IPersister<Employee> pm = PersisterFactory.create();
+
 	@Test
 	public void run() {
 		doRun();
 	}
+
 	public void doRun() {
-		String getParentId ="SELECT pkid from uc_organization where pid=0";
+		String getParentId = "SELECT pkid from uc_organization where pid=0";
 		DataTable getDt = organizationService.executeTable(getParentId.toString(), null);
 		for (IRow row : getDt) {
 			Integer getPkid = Integer.parseInt(row.getString("pkid"));
 			setOrfanization(getPkid);
 		}
 	}
+
 	private void setOrfanization(Integer pid) {
-		String getParentId ="SELECT pkid from uc_organization where pid="+pid;
+		String getParentId = "SELECT pkid from uc_organization where pid=" + pid;
 		DataTable getDt = organizationService.executeTable(getParentId.toString(), null);
-		if(getDt.size()>0){
+		if (getDt.size() > 0) {
 			for (IRow row : getDt) {
-				setOrfanization(Integer.parseInt(row.getString("pkid")));  
-	        }  	
-		}else{
-			String updateSql = "UPDATE uc_organization set is_leaf=1 where pkid="+pid;
+				setOrfanization(Integer.parseInt(row.getString("pkid")));
+			}
+		} else {
+			String updateSql = "UPDATE uc_organization set is_leaf=1 where pkid=" + pid;
 			int i = pm.executeNonQuery(updateSql.toString(), null);
 		}
-		
-    }  
+
+	}
 }
