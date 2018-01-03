fef4b0c8c25d4ba368568267ee3bbae0c4a3addf
zhangchao <chaozhang@gongsibao.com>
2018-01-03 17:36:24 +0800

    fix

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/basic/workspace/u8/SetOfBooksWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/basic/workspace/u8/SetOfBooksWorkspaceTest.java
@@ -15,25 +15,25 @@ import org.netsharp.resourcenode.entity.ResourceNode;
 
 import com.gongsibao.entity.u8.SetOfBooks;
 
-public class SetOfBooksWorkspaceTest extends WorkspaceCreationBase  {
-	
+public class SetOfBooksWorkspaceTest extends WorkspaceCreationBase {
+
 	@Before
 	public void setup() {
 
-		entity = SetOfBooks.class;//实体
-		urlList = "/basic/u8/setofBbooks/list";//列表的url
-		urlForm = "/basic/u8/setofBbooks/form";//弹出框的url
+		entity = SetOfBooks.class;// 实体
+		urlList = "/basic/u8/setofBbooks/list";// 列表的url
+		urlForm = "/basic/u8/setofBbooks/form";// 弹出框的url
 		listPartName = formPartName = "账套信息";
-		meta = MtableManager.getMtable(entity);//获取实体元数据
+		meta = MtableManager.getMtable(entity);// 获取实体元数据
 		formPartName = listPartName = meta.getName();
-		resourceNodeCode = "GSB_Basic_U8_"+SetOfBooks.class.getSimpleName();//菜单节点码（名称）
-		
-		formOpenMode = OpenMode.WINDOW;//编辑框打开的形式
+		resourceNodeCode = "GSB_Basic_U8_" + SetOfBooks.class.getSimpleName();// 菜单节点码（名称）
+
+		formOpenMode = OpenMode.WINDOW;// 编辑框打开的形式
 		openWindowWidth = 800;
 		openWindowHeight = 450;
 	}
 
-	//默认的grid信息的配置
+	// 默认的grid信息的配置
 	protected PDatagrid createDatagrid(ResourceNode node) {
 
 		PDatagrid datagrid = super.createDatagrid(node);
@@ -42,35 +42,41 @@ public class SetOfBooksWorkspaceTest extends WorkspaceCreationBase  {
 			datagrid.setResourceNode(node);
 			datagrid.setName("账套信息列表");
 		}
-		
+
 		PDatagridColumn column = null;
 		addColumn(datagrid, "name", "名称", ControlTypes.TEXT_BOX, 200);
 		addColumn(datagrid, "senderNo", "外部系统编号", ControlTypes.TEXT_BOX, 80);
-		column = addColumn(datagrid, "type", "类型", ControlTypes.ENUM_BOX, 100);{
+		column = addColumn(datagrid, "type", "类型", ControlTypes.ENUM_BOX, 100);
+		{
 			column.setAlign(DatagridAlign.CENTER);
 		}
-		addColumn(datagrid, "taxRate", "税率", ControlTypes.DECIMAL_BOX, 100);	
+		column = addColumn(datagrid, "taxRate", "税率", ControlTypes.DECIMAL_BOX, 100);
+		{
+			column.setPrecision(4);
+		}
 		addColumn(datagrid, "sort", "排序编号", ControlTypes.NUMBER_BOX, 100);
 		addColumn(datagrid, "accountCode", "科目编码", ControlTypes.TEXT_BOX, 150);
 		addColumn(datagrid, "enterName", "默认的制单人", ControlTypes.TEXT_BOX, 100);
-		column = addColumn(datagrid, "abbreviation", "简称", ControlTypes.TEXT_BOX, 200);{
+		column = addColumn(datagrid, "abbreviation", "简称", ControlTypes.TEXT_BOX, 200);
+		{
 			column.setAlign(DatagridAlign.CENTER);
-		}		
+		}
 		addColumn(datagrid, "enabled", "是否可用", ControlTypes.BOOLCOMBO_BOX, 50);
 		addColumn(datagrid, "creator", "添加人", ControlTypes.TEXT_BOX, 100);
 		addColumn(datagrid, "createTime", "添加时间", ControlTypes.DATETIME_BOX, 20);
 		return datagrid;
 	}
 
-	//默认的表单配置信息
+	// 默认的表单配置信息
 	protected PForm createForm(ResourceNode node) {
 
 		PForm form = super.createForm(node);
 		form.setColumnCount(2);
-		addFormField(form, "name", "名称", null, ControlTypes.TEXT_BOX, true, false);		
+		addFormField(form, "name", "名称", null, ControlTypes.TEXT_BOX, true, false);
 		addFormField(form, "senderNo", "外部系统编号", null, ControlTypes.TEXT_BOX, true, false);
 		addFormField(form, "type", "类型", null, ControlTypes.ENUM_BOX, true, false);
-		PFormField field = addFormField(form, "taxRate", "税率", null, ControlTypes.DECIMAL_BOX, true, false);		{
+		PFormField field = addFormField(form, "taxRate", "税率", null, ControlTypes.DECIMAL_BOX, true, false);
+		{
 			field.setPrecision(3);
 		}
 		addFormField(form, "sort", "排序编号", null, ControlTypes.NUMBER_BOX, true, false);
@@ -80,15 +86,15 @@ public class SetOfBooksWorkspaceTest extends WorkspaceCreationBase  {
 		addFormField(form, "enabled", "是否可用", null, ControlTypes.SWITCH_BUTTON, true, false);
 		return form;
 	}
-	
-	//默认的表单操作
+
+	// 默认的表单操作
 	@Override
 	protected void doOperation() {
-		
+
 		ResourceNode node = this.getResourceNode();
-		operationService.addOperation(node,OperationTypes.view);
-		operationService.addOperation(node,OperationTypes.add);
-		operationService.addOperation(node,OperationTypes.update);
-		operationService.addOperation(node,OperationTypes.delete);
+		operationService.addOperation(node, OperationTypes.view);
+		operationService.addOperation(node, OperationTypes.add);
+		operationService.addOperation(node, OperationTypes.update);
+		operationService.addOperation(node, OperationTypes.delete);
 	}
 }
