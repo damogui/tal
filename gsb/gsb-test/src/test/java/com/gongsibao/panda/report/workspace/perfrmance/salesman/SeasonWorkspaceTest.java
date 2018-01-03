a5c25963def3ba6cdf7449cfcbd093760b054fed
yanxuebiao <xbyan@gongsibao.com>
2018-01-03 19:22:05 +0800

    添加业务员过滤

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/report/workspace/perfrmance/salesman/SeasonWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/report/workspace/perfrmance/salesman/SeasonWorkspaceTest.java
@@ -75,6 +75,10 @@ public class SeasonWorkspaceTest extends WorkspaceCreationBase {
 		item = addRefrenceQueryItem(queryProject, "department.shortName", "部门", "Gsb_Organization");{
 			item.setRequired(true);
 		}
+		item = addQueryItem(queryProject, "salesman.name", "业务员", ControlTypes.TEXT_BOX);
+		{
+			item.setRequired(false);
+		}
 		return queryProject;
 	}
 
