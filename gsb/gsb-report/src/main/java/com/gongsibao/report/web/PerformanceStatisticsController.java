7436e923c535788be012b18b64171de040b1bf1a
hanwei <hwhping@163.com>
2018-01-04 20:12:14 +0800

    生成报表时可选择日期

--- gsb/gsb-report/src/main/java/com/gongsibao/report/web/PerformanceStatisticsController.java
+++ gsb/gsb-report/src/main/java/com/gongsibao/report/web/PerformanceStatisticsController.java
@@ -8,9 +8,9 @@ import org.netsharp.panda.commerce.TreegridPart;
 import com.gongsibao.report.base.IPerformanceStatisticsService;
 
 public class PerformanceStatisticsController extends TreegridPart{
-	public void execute(){
+	public void execute(Date date){
+		
 		IPerformanceStatisticsService service = ServiceFactory.create(IPerformanceStatisticsService.class);
-		Date date = new Date();
 		int[] departmentIdIds = { 4, 5 };//汉唐信通，供应商
 		service.generate(date,departmentIdIds);
 	}
