271a5d9cc361f287dee1c3464a5443de445fc580
yanxuebiao <xbyan@gongsibao.com>
2018-01-04 13:52:37 +0800

    全部订单

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/trade/workspace/order/AllOrderWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/trade/workspace/order/AllOrderWorkspaceTest.java
@@ -38,18 +38,17 @@ public class AllOrderWorkspaceTest extends WorkspaceCreationBase{
 		PDatagrid datagrid = super.createDatagrid(node);
 		PDatagridColumn column = null;
 		column = addColumn(datagrid, "no", "订单编号", ControlTypes.TEXT_BOX, 100,true);
-		column = addColumn(datagrid, "channelOrderNo", "渠道订单号", ControlTypes.TEXT_BOX, 100,true);		
-		addColumn(datagrid, "payTime", "支付时间", ControlTypes.DATE_BOX, 130);
-		column = addColumn(datagrid, "payStatusId", "支付状态", ControlTypes.NUMBER_BOX, 90);
-		column = addColumn(datagrid, "processStatusId", "执行进度", ControlTypes.NUMBER_BOX, 90);
-		column = addColumn(datagrid, "refundStatusId", "退款状态", ControlTypes.NUMBER_BOX, 90);		
+		column = addColumn(datagrid, "channelOrderNo", "渠道订单号", ControlTypes.TEXT_BOX, 100,true);
+		column = addColumn(datagrid, "payStatus.name", "支付状态", ControlTypes.NUMBER_BOX, 90);
+		column = addColumn(datagrid, "processStatus.name", "执行进度", ControlTypes.NUMBER_BOX, 90);
+		column = addColumn(datagrid, "refundStatus.name", "退款状态", ControlTypes.NUMBER_BOX, 90);		
 		column = addColumn(datagrid, "totalPrice", "总金额", ControlTypes.NUMBER_BOX, 90);
 		column = addColumn(datagrid, "payablePrice", "未支付金额", ControlTypes.NUMBER_BOX, 90);
 		column = addColumn(datagrid, "paidPrice", "已支付金额", ControlTypes.NUMBER_BOX, 90);
 		column = addColumn(datagrid, "isInstallment", "分期付款", ControlTypes.NUMBER_BOX, 90);
 		addColumn(datagrid, "accountName", "客户名称", ControlTypes.TEXT_BOX, 100);
 		column = addColumn(datagrid, "accountMobile", "手机号", ControlTypes.TEXT_BOX, 90);
-		column = addColumn(datagrid, "platformSource", "订单来源", ControlTypes.NUMBER_BOX, 90);
+		column = addColumn(datagrid, "platformSourceDict.name", "订单来源", ControlTypes.NUMBER_BOX, 90);
 		addColumn(datagrid, "payTime", "支付时间", ControlTypes.DATE_BOX, 130);
 		addColumn(datagrid, "addTime", "创建时间", ControlTypes.DATE_BOX, 130);
 		column = addColumn(datagrid, "type", "订单类型", ControlTypes.TEXT_BOX, 100, true);
@@ -57,7 +56,7 @@ public class AllOrderWorkspaceTest extends WorkspaceCreationBase{
 		{
 			column.setFormatter("return '<span title='+value+'>'+value+'</span>'");
 		}
-		column = addColumn(datagrid, "sourceTypeId", "来源类型", ControlTypes.NUMBER_BOX, 90);
+		column = addColumn(datagrid, "sourceType.name", "来源类型", ControlTypes.NUMBER_BOX, 90);
 		column = addColumn(datagrid, "processStatus.name", "订单状态", ControlTypes.TEXT_BOX, 80);
 		{
 			column.setAlign(DatagridAlign.CENTER);
