d3ee275be0a99b7daadcb7346a2cd898ede5d8c6
jy <jy@163.com>
2018-01-11 12:42:07 +0800

    rm crm

--- gsb/gsb-igirl/src/main/java/com/gongsibao/crm/base/ICompanySellIndustryMapService.java
+++
@@ -1,8 +0,0 @@
-//package com.gongsibao.crm.base;
-//
-//import org.netsharp.base.IPersistableService;
-//
-//import com.gongsibao.entity.crm.CompanySellIndustryMap;
-//
-//public interface ICompanySellIndustryMapService extends IPersistableService<CompanySellIndustryMap> {
-//}
--- gsb/gsb-igirl/src/main/java/com/gongsibao/crm/base/ICompanySellService.java
+++
@@ -1,8 +0,0 @@
-//package com.gongsibao.crm.base;
-//
-//import org.netsharp.base.IPersistableService;
-//
-//import com.gongsibao.entity.crm.CompanySell;
-//
-//public interface ICompanySellService extends IPersistableService<CompanySell> {
-//}
