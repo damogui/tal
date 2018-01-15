97d2f061a22c5d8b0facdc9e04dbedee95bf55c9
hanwei <hwhping@163.com>
2018-01-15 19:49:42 +0800

    1111

--- gsb/gsb-report/src/main/java/com/gongsibao/report/job/IPerfrmanceJob.java
+++ gsb/gsb-report/src/main/java/com/gongsibao/report/job/IPerfrmanceJob.java
@@ -1,6 +1,5 @@
 package com.gongsibao.report.job;
 
-import java.util.Calendar;
 import java.util.Date;
 
 import org.netsharp.communication.ServiceFactory;
@@ -16,10 +15,10 @@ public class IPerfrmanceJob implements IJob {
 		IPerformanceStatisticsService service = ServiceFactory.create(IPerformanceStatisticsService.class);
 		// 跑批的前一天时间
 		Date date = new Date();
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(date);
-		calendar.add(Calendar.DAY_OF_MONTH, -1);
-		date = calendar.getTime();
+//		Calendar calendar = Calendar.getInstance();
+//		calendar.setTime(date);
+//		calendar.add(Calendar.DAY_OF_MONTH, -1);
+//		date = calendar.getTime();
 		int[] departmentIds = { 337, 4, 5 };// 股转中心，汉唐信通，供应商 
 		service.generate(date, departmentIds);
 	}
