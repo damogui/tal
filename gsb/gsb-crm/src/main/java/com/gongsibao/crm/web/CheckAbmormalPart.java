e2a3c244fa0406e281f8b81adc89b3433762f326
yanxuebiao <xbyan@gongsibao.com>
2018-01-18 18:56:36 +0800

    1

--- gsb/gsb-crm/src/main/java/com/gongsibao/crm/web/CheckAbmormalPart.java
+++ gsb/gsb-crm/src/main/java/com/gongsibao/crm/web/CheckAbmormalPart.java
@@ -1,7 +1,15 @@
 package com.gongsibao.crm.web;
 
+import org.netsharp.communication.ServiceFactory;
 import org.netsharp.panda.commerce.ListPart;
+
+import com.gongsibao.crm.base.INCustomerTaskService;
 
 public class CheckAbmormalPart extends ListPart{
 
+	//修改审核状态
+	public int taskInspectionState(Integer taskId,Integer selectValue,String getNote){
+		INCustomerTaskService taskService = ServiceFactory.create(INCustomerTaskService.class);
+		return taskService.updateInspectionState(taskId, selectValue, getNote);
+	}
 }
