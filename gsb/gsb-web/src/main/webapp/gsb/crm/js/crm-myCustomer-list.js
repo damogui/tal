bc490dafe76265fa9670d136c90244859d1bc8f9
yanxuebiao <xbyan@gongsibao.com>
2018-01-24 15:12:16 +0800

    1

---
+++ gsb/gsb-web/src/main/webapp/gsb/crm/js/crm-myCustomer-list.js
@@ -0,0 +1,22 @@
+//无法签单中的工具栏
+System.Declare("com.gongsibao.crm.web");
+com.gongsibao.crm.web.MyCustomerListPart = org.netsharp.panda.commerce.ListPart.Extends({
+	ctor : function() {
+		this.base();
+	},
+	add : function(){
+		var url='/panda/operation/customer/add?openType=0';
+		layer.open({
+	  		  type: 2,
+	  		  title: '添加客户',
+	  		  fixed: false,
+	  		  maxmin: true,
+	  		  shadeClose:true,
+	  		  area: ['90%','90%'],
+	  		  content: url,
+	  		  cancel: function(){ 
+
+			  }
+	  	    });
+	}
+});
