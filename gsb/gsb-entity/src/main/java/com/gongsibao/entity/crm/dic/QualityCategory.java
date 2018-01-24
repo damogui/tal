d0b3f51fbccded0c676f2f50a7137cd7c6cceb8e
gj <jjguo@gongsibao.com>
2018-01-24 14:08:52 +0800

    插入NCustomerTaskQuality单元实例

--- gsb/gsb-entity/src/main/java/com/gongsibao/entity/crm/dic/QualityCategory.java
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/crm/dic/QualityCategory.java
@@ -5,13 +5,13 @@ import org.netsharp.base.IEnum;
 
 public enum QualityCategory implements IEnum {
 
-	A(1, "A类"), 
-	B(2, "B类"), 
-	C(3, "C类"), 
-	D(4, "D类");
-	D(4, "D类"),
+    A(1, "A类"),
+    B(2, "B类"),
+    C(3, "C类"),
+    D(4, "D类"),
     X(5, "X类"),
-    S(6, "S类");	private int value;
+    S(6, "S类");
+    private int value;
 	private String text;
 
 	QualityCategory(int value, String text) {
