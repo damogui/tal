8f433dedfc8048931200245a7dbe59d11d37a148
hanwei <hwhping@163.com>
2018-01-27 16:08:34 +0800

    增加任务新增，修改的Action

---
+++ gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/add/ActionAddVerify.java
@@ -0,0 +1,18 @@
+package com.gongsibao.crm.service.action.add;
+
+import org.netsharp.action.ActionContext;
+import org.netsharp.action.IAction;
+
+/**
+ * @author hw
+ * 新增任务校验
+ */
+public class ActionAddVerify implements IAction{
+
+	@Override
+	public void execute(ActionContext ctx) {
+	
+		
+	}
+
+}
