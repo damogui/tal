81456fa8c66dd774be1e8050f33aa115be2c9dda
hanwei <hwhping@163.com>
2018-01-29 16:16:46 +0800

    【完成】5.客户：增加任务数量字段。 创建任务时回写此值

---
+++ gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/task/save/ActionSaveWriteBack.java
@@ -0,0 +1,41 @@
+package com.gongsibao.crm.service.action.task.save;
+
+import org.netsharp.action.ActionContext;
+import org.netsharp.action.IAction;
+import org.netsharp.core.EntityState;
+import org.netsharp.core.MtableManager;
+import org.netsharp.persistence.IPersister;
+import org.netsharp.persistence.PersisterFactory;
+
+import com.gongsibao.entity.crm.NCustomer;
+import com.gongsibao.entity.crm.NCustomerTask;
+
+/**
+ * @ClassName: ActionSaveWriteBack
+ * @Description:TODO 新增任务：回写客户taskCount
+ * @author: 韩伟
+ * @date: 2018年1月29日 下午4:10:02
+ * 
+ * @Copyright: 2018 www.yikuaxiu.com Inc. All rights reserved.
+ */
+public class ActionSaveWriteBack implements IAction {
+
+	@Override
+	public void execute(ActionContext ctx) {
+
+		NCustomerTask task = (NCustomerTask) ctx.getItem();
+		EntityState state = ctx.getState();
+		if (state == EntityState.New) {
+
+			this.updateCustomerTaskCount(task.getCustomerId());
+		}
+	}
+
+	private void updateCustomerTaskCount(Integer customerId) {
+
+		String tableName = MtableManager.getMtable(NCustomer.class).getTableName();
+		String cmdText = String.format("update %s set task_count=task_count+1 where id=%d", tableName, customerId);
+		IPersister<NCustomerTask> pm = PersisterFactory.create();
+		pm.executeNonQuery(cmdText, null);
+	}
+}
