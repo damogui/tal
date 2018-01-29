a5cc26bd4c8341c05c056129e8b57d3bdd9ad7d7
gj <jjguo@gongsibao.com>
2018-01-29 16:01:27 +0800

    seo

--- gsb/gsb-web/src/main/webapp/gsb/crm/sys/js/sys-salesman-tree-part.js
+++ gsb/gsb-web/src/main/webapp/gsb/crm/sys/js/sys-salesman-tree-part.js
@@ -7,29 +7,18 @@ com.gongsibao.crm.web.SysSalesmanTreePart =org.netsharp.panda.commerce.TreePart.
 
     ctor: function () {
         this.base();
-    },
-   // addExtraParams:function(urls){//进行扩展
-   //
-   //      var supplierId = this.queryString('supplierId');
-   //      if(supplierId){
-   //
-   //          urls.push("supplierId=" + supplierId);
-   //      }
-   //  }
+    }
 });
 
 
 //重新调用查询
 $(function() {
-debugger;
     var supplierId = controllerGsbCrmSysDepartmentTree.queryString('supplierId');
-    var url = '/panda/rest/service?vid='+controllerGsbCrmSysDepartmentTree.context.vid+'&method=query&filter=';
-    if(supplierId){
-        url+='&supplierId='+supplierId;
-        $('#treeGsbCrmSysDepartmentTree').tree('options').url=url;
-    }
-    //列表
-    $('#treeGsbCrmSysDepartmentTree').tree('reload');//进行重新加载
+    var params = {
+        supplierId: supplierId
 
+    };
+    $('#treeGsbCrmSysDepartmentTree').tree("options").queryParams = params;
+    $('#treeGsbCrmSysDepartmentTree').tree('reload');
 
 });
