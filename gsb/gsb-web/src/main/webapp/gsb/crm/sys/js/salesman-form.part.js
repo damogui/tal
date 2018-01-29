650fd7edce0a9a3cde23998220639a352ff3606c
hanwei <hwhping@163.com>
2018-01-29 12:04:53 +0800

    优化

--- gsb/gsb-web/src/main/webapp/gsb/crm/sys/js/salesman-form.part.js
+++ gsb/gsb-web/src/main/webapp/gsb/crm/sys/js/salesman-form.part.js
@@ -4,29 +4,35 @@ com.gongsibao.crm.web.SalesmaProductDetailPart = org.netsharp.panda.commerce.Det
         this.base();
     },
     productCategory1Select:function(record){
-    	
-    	//alert(record.id );	
+
     	//加载二级分类
         this.invokeService("queryByProductCategoryId1", [record.id], function (data) {
         	
-
+        	$('#productCategory2_name').combobox('clear').combobox('loadData',data);
         });
     },
     productCategory2Select:function(record){
     	
-    	//反写一级分类，加载产品
-        this.invokeService("queryByProductCategoryId2", [record.id], function (data) {
-        	
-
-        });
+    	try{
+    		
+        	$('#product_name').combogrid('clear');
+    		var grid = $('#product_name').combogrid('grid');
+    		var options = $(grid).datagrid('options');
+    		var filter = ' enabled____1 and type_id____'+record.id;
+    		options.url = '\/panda\/rest\/reference?code=CRM_Product&filter='+ filter;
+    		$(grid).datagrid(options);	
+    		
+    	}catch(ex){
+    		
+    	}
     },
     productChange:function(newValue,oldValue){
     	
     	//判断一级分类，二级分类是否为空，如果为空，则
     	//反写一级分类、二级分类
-        this.invokeService("queryByProductId", [newValue], function (data) {
-        	
-
-        });
+//        this.invokeService("queryByProductId", [newValue], function (data) {
+//        	
+//
+//        });
     }
 });
