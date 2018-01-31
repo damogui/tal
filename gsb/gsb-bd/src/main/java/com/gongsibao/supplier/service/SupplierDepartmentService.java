f92196cc71e6da3fa9f96ccbf5ba99eb31630261
gj <jjguo@gongsibao.com>
2018-01-31 19:19:33 +0800

    删除的时候进行校验是不是存在底层数据

--- gsb/gsb-bd/src/main/java/com/gongsibao/supplier/service/SupplierDepartmentService.java
+++ gsb/gsb-bd/src/main/java/com/gongsibao/supplier/service/SupplierDepartmentService.java
@@ -8,6 +8,7 @@ import org.netsharp.communication.Service;
 import org.netsharp.core.BusinessException;
 import org.netsharp.core.EntityState;
 import org.netsharp.core.Oql;
+import org.netsharp.core.QueryParameters;
 import org.netsharp.util.sqlbuilder.UpdateBuilder;
 
 import com.gongsibao.bd.service.SupplierPersistableService;
@@ -18,95 +19,109 @@ import com.gongsibao.supplier.base.ISupplierDepartmentService;
 @Service
 public class SupplierDepartmentService extends SupplierPersistableService<SupplierDepartment> implements ISupplierDepartmentService {
 
-	public SupplierDepartmentService() {
-		super();
-		this.type = SupplierDepartment.class;
-	}
+    public SupplierDepartmentService() {
+        super();
+        this.type = SupplierDepartment.class;
+    }
 
-	public SupplierDepartment save(SupplierDepartment entity) {
+    public SupplierDepartment save(SupplierDepartment entity) {
 
-		EntityState state = entity.getEntityState();
+        EntityState state = entity.getEntityState();
 
-		if (state == EntityState.Deleted) {
+        if (state == EntityState.Deleted) {
 
-			this.deleteVerify(entity);
-		}else{
+            int salesmanCount = this.deleteVerify(entity);
+            if (salesmanCount > 0) {//校验能不能删除
 
-            SupplierService   supplierService=new SupplierService();
-            Supplier supplier=supplierService.byId(entity.getSupplierId());
-            if (supplier==null){
+                throw new BusinessException("部门下面有员工不能删除");
+
+            } else {//为0的能删除
+
+                entity = super.save(entity);
+            }
+
+        } else {
+
+            SupplierService supplierService = new SupplierService();
+            Supplier supplier = supplierService.byId(entity.getSupplierId());
+            if (supplier == null) {
                 throw new BusinessException("服务商属性不正确");
             }
             entity.setType(supplier.getType());//设置平台属性
+
+            entity = super.save(entity);
+            this.updateIsLeaf(entity);
+
         }
 
 
-		entity = super.save(entity);
+        return entity;
+    }
 
-		if (state != EntityState.Deleted) {
+    /**
+     * @throws
+     * @Title: deleteVerify
+     * @Description: TODO(删除校验)
+     * @param: @param entity
+     * @return: void
+     */
+    private int deleteVerify(SupplierDepartment entity) {
 
-			this.updateIsLeaf(entity);
-		}
-		return entity;
-	}
+        // 校验是否有下级，是否有员工
 
-	/**
-	 * @Title: deleteVerify
-	 * @Description: TODO(删除校验)
-	 * @param: @param entity
-	 * @return: void
-	 * @throws
-	 */
-	private void deleteVerify(SupplierDepartment entity) {
 
-		// 校验是否有下级，是否有员工
+        String sql = "SELECT  COUNT(1) FROM sp_salesman  WHERE  department_id  =?";
+        QueryParameters qps = new QueryParameters();
+        qps.add("@department_id", entity.getId(), Types.INTEGER);
+        int num = Integer.parseInt(this.pm.executeScalar(sql, qps).toString());
+        return num;
 
-	}
+    }
 
-	/**
-	 * @Title: updateIsLeaf
-	 * @Description: TODO(更新末节点)
-	 * @param: @param entity
-	 * @return: void
-	 * @throws
-	 */
-	private void updateIsLeaf(SupplierDepartment entity) {
+    /**
+     * @throws
+     * @Title: updateIsLeaf
+     * @Description: TODO(更新末节点)
+     * @param: @param entity
+     * @return: void
+     */
+    private void updateIsLeaf(SupplierDepartment entity) {
 
-		if (entity.getParentId() != null) {
+        if (entity.getParentId() != null) {
 
-			UpdateBuilder updateSql = UpdateBuilder.getInstance();
-			{
-				updateSql.update("sp_department");
-				updateSql.set("is_leaf", false);
-				updateSql.where("id =" + entity.getParentId());
-			}
-			String cmdText = updateSql.toSQL();
-			pm.executeNonQuery(cmdText, null);
-		}
-	}
+            UpdateBuilder updateSql = UpdateBuilder.getInstance();
+            {
+                updateSql.update("sp_department");
+                updateSql.set("is_leaf", false);
+                updateSql.where("id =" + entity.getParentId());
+            }
+            String cmdText = updateSql.toSQL();
+            pm.executeNonQuery(cmdText, null);
+        }
+    }
 
-	@Override
-	public List<Integer> getSubDepartmentIdList(Integer departmentId) {
+    @Override
+    public List<Integer> getSubDepartmentIdList(Integer departmentId) {
 
-		List<Integer> idList = new ArrayList<Integer>();
-		Oql oql = new Oql();
-		{
-			oql.setType(this.type);
-			oql.setSelects("id");
-			oql.setFilter("parentId=?");
-			oql.getParameters().add("@parentId", departmentId, Types.INTEGER);
-		}
-		
-		List<SupplierDepartment> list = this.queryList(oql);
-		for(SupplierDepartment entity :list){
-			
-			idList.add(entity.getId());
-			List<Integer> subIdList = getSubDepartmentIdList(entity.getId());
-			if(idList.size()>0){
-				idList.addAll(subIdList);
-			}
-		}
-		
-		return idList;
-	}
+        List<Integer> idList = new ArrayList<Integer>();
+        Oql oql = new Oql();
+        {
+            oql.setType(this.type);
+            oql.setSelects("id");
+            oql.setFilter("parentId=?");
+            oql.getParameters().add("@parentId", departmentId, Types.INTEGER);
+        }
+
+        List<SupplierDepartment> list = this.queryList(oql);
+        for (SupplierDepartment entity : list) {
+
+            idList.add(entity.getId());
+            List<Integer> subIdList = getSubDepartmentIdList(entity.getId());
+            if (idList.size() > 0) {
+                idList.addAll(subIdList);
+            }
+        }
+
+        return idList;
+    }
 }
