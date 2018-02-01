97c098b240a128d1675fa825bacd791815119730
jy <jy@163.com>
2018-02-01 21:06:32 +0800

    d

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/igirl/workspace/MyAllTradeMarkCaseWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/igirl/workspace/MyAllTradeMarkCaseWorkspaceTest.java
@@ -110,6 +110,8 @@ public class MyAllTradeMarkCaseWorkspaceTest extends WorkspaceCreationBase {
 		PDatagridColumn column = null;
 		addColumn(datagrid, "id", "操作", ControlTypes.OPERATION_COLUMN, 100, true);
 		addColumn(datagrid, "code", "编号", ControlTypes.TEXT_BOX, 150, true);
+		addColumn(datagrid, "companyName", "公司名称", ControlTypes.TEXT_BOX, 200);
+		addColumn(datagrid, "applier", "申请人", ControlTypes.TEXT_BOX, 200);
 		addColumn(datagrid, "urgency", "紧急程度(小时)", ControlTypes.TEXT_BOX, 100);
 		addColumn(datagrid, "creator", "业务员", ControlTypes.TEXT_BOX, 50);
 	
