2f0fa4b52d42f1d2991af583f07c1243a13aa9c4
gj <jjguo@gongsibao.com>
2018-02-02 09:48:39 +0800

    优化

--- gsb/gsb-api/src/main/java/com/gongsibao/api/conroller/ma/SellingController.java
+++ gsb/gsb-api/src/main/java/com/gongsibao/api/conroller/ma/SellingController.java
@@ -21,22 +21,22 @@ import java.util.List;
  * Created by win on 2018/2/1.
  */
 
-@Path("/selling")
+@Path("/ma/selling")
 public class SellingController {
 
     //ISellingDemandService sellingDemandServiceI = ServiceFactory.create(ISellingDemandService.class);
     SellingDemandDTOService sellingDemandDTOService = new SellingDemandDTOService();
-    private HttpServletRequest request;
-    private QuerySellingDemandDTO para;
+//    private HttpServletRequest request;
+//    private QuerySellingDemandDTO para;
 
 
-  /*列表接口*/
+    /*列表接口*/
     @GET
-    @Path("/sellinglist")
+    @Path("/getsellinglist")
     @Produces(MediaType.APPLICATION_JSON)
     public List<SellingDemandDTO> getSellingList(@Context HttpServletRequest request) throws IOException {
         String para = request.getParameter("para");
-        QuerySellingDemandDTO paraM= (QuerySellingDemandDTO) org.netsharp.util.JsonManage.deSerialize(QuerySellingDemandDTO.class,para);
+        QuerySellingDemandDTO paraM = (QuerySellingDemandDTO) org.netsharp.util.JsonManage.deSerialize(QuerySellingDemandDTO.class, para);
         //QuerySellingDemandDTO paraM = (QuerySellingDemandDTO) JSON.parseObject(para, QuerySellingDemandDTO.class);
 
         List<SellingDemandDTO> list = sellingDemandDTOService.queryList(paraM);
@@ -45,26 +45,26 @@ public class SellingController {
 
     /*服务类型*/
     @GET
-    @Path("/servicetype")
+    @Path("/getservicetype")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<SellingDemandDTO>  getServiceType(@Context HttpServletRequest request) throws IOException {
+    public List<SellingDemandDTO> getServiceType(@Context HttpServletRequest request) throws IOException {
         String para = request.getParameter("para");
-        QuerySellingDemandDTO paraM= (QuerySellingDemandDTO) org.netsharp.util.JsonManage.deSerialize(QuerySellingDemandDTO.class,para);
+        QuerySellingDemandDTO paraM = (QuerySellingDemandDTO) org.netsharp.util.JsonManage.deSerialize(QuerySellingDemandDTO.class, para);
         List<SellingDemandDTO> list = sellingDemandDTOService.queryList(paraM);
         return list;
     }
 
     /*全国地区*/
     @GET
-    @Path("/pcc")
+    @Path("/getprovincecitycounty")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<ProvinceCityCountyDTO> getPcc(@Context HttpServletRequest request){
+    public List<ProvinceCityCountyDTO> getProvinceCityCounty(@Context HttpServletRequest request) {
 
         Integer parentId = 0;
         String para = request.getParameter("para");
-        if (!StringManager.isNullOrEmpty(para)){
+        if (!StringManager.isNullOrEmpty(para)) {
 
-            parentId=Integer.parseInt(para);
+            parentId = Integer.parseInt(para);
         }
         List<ProvinceCityCountyDTO> list = sellingDemandDTOService.getPcc(parentId);
         return list;
@@ -72,11 +72,11 @@ public class SellingController {
 
     /*筛选*/
     @GET
-    @Path("/filters")
+    @Path("/getfilters")
     @Produces(MediaType.APPLICATION_JSON)
     public List<SellingDemandDTO> getFilters(@Context HttpServletRequest request) throws IOException {
         String para = request.getParameter("para");
-        QuerySellingDemandDTO paraM= (QuerySellingDemandDTO) org.netsharp.util.JsonManage.deSerialize(QuerySellingDemandDTO.class,para);
+        QuerySellingDemandDTO paraM = (QuerySellingDemandDTO) org.netsharp.util.JsonManage.deSerialize(QuerySellingDemandDTO.class, para);
         List<SellingDemandDTO> list = sellingDemandDTOService.queryList(paraM);
         return list;
     }
@@ -84,13 +84,18 @@ public class SellingController {
 
     /*详情*/
     @GET
-    @Path("/details/{sellingId}")
+    @Path("/getDetails/")
     @Produces(MediaType.APPLICATION_JSON)
-    public SellingDemandDTO getDetails(@PathParam("sellingId") int sellingId) {
+    public SellingDemandDTO getDetails(@Context HttpServletRequest request) {
+        String para = request.getParameter("para");
+        Integer sellingId = 0;
+        if (!StringManager.isNullOrEmpty(para)) {
+
+            sellingId=Integer.parseInt(para);
+        }
         SellingDemandDTO sellingDemandDTO = sellingDemandDTOService.getSellingDemandDTOById(sellingId);
         return sellingDemandDTO;
     }
-
 
 
 }
