c2cfb77e8803379763fcdcd3486131776a06e22a
hanwei <hwhping@163.com>
2018-02-03 18:02:04 +0800

    BUG

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/operation/workspace/crm/form/CustomerAddWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/operation/workspace/crm/form/CustomerAddWorkspaceTest.java
@@ -213,7 +213,8 @@ public class CustomerAddWorkspaceTest extends WorkspaceCreationBase {
 
 		ResourceNode node = this.resourceService.byCode(companysResourceNodeCode);
 		PDatagrid datagrid = new PDatagrid(node, "关联企业");
-		{
+		{	
+			addColumn(datagrid, "createTime", "关联时间", ControlTypes.DATETIME_BOX, 130);
 			addColumn(datagrid, "company.companyName", "公司名称", ControlTypes.TEXT_BOX, 300);
 		}
 
