812e41e654d6e3b39bbd3b561a8b30579945ebe7
王昆 <wangkun@gongsibao.com>
2018-02-06 19:17:36 +0800

    大数据接口对接

---
+++ gsb/gsb-taurus-sdk/src/main/java/com/gongsibao/taurus/api/TmRegAssembleListApi.java
@@ -0,0 +1,41 @@
+package com.gongsibao.taurus.api;
+
+import com.gongsibao.taurus.message.response.TmAssembleResponseMessage;
+
+import java.util.List;
+
+/**
+ * 已有商标（聚合）
+ */
+public class TmRegAssembleListApi extends AbstractApi<TmAssembleResponseMessage> {
+
+    /**
+     * 公司名称
+     */
+    private String name;
+
+    @Override
+    protected void setExtendParameter(List<String> parameters) {
+        if (null != parameters) {
+            parameters.add("name=" + this.getName());
+        }
+    }
+
+    @Override
+    protected String getUrl() {
+        return "/dataapi/v3/ic/getTmNameListByCompanyName";
+    }
+
+    @Override
+    public Class<?> getResponseType() {
+        return TmAssembleResponseMessage.class;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
