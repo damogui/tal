1ebba50d921728810ccc03f3e2afece30bdc14d5
yanxuebiao <xbyan@gongsibao.com>
2018-02-06 17:29:16 +0800

    1

--- gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/task/allocation/manual/ActionManualAllocationVerify.java
+++ gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/task/allocation/manual/ActionManualAllocationVerify.java
@@ -2,36 +2,47 @@ package com.gongsibao.crm.service.action.task.allocation.manual;
 
 import org.netsharp.action.ActionContext;
 import org.netsharp.action.IAction;
-import org.netsharp.communication.ServiceFactory;
 
 import com.gongsibao.entity.crm.NCustomerTask;
-import com.gongsibao.entity.supplier.Salesman;
-import com.gongsibao.supplier.base.ISalesmanService;
+import com.gongsibao.entity.crm.dic.AllocationState;
+import com.gongsibao.utils.SalesmanOrganization;
+import com.gongsibao.utils.SupplierSessionManager;
 
 /**
- * @author hw 分配校验
+ * @author hw
+ * 分配校验
  */
-public class ActionManualAllocationVerify implements IAction {
+public class ActionManualAllocationVerify implements IAction{
 
 	@Override
 	public void execute(ActionContext ctx) {
 		
-		// 服务商和部门如果不选择，此时根据业务员Id,获取相应的服务商和部门
-		NCustomerTask taskEntity = (NCustomerTask) ctx.getItem();
-		ISalesmanService salesmanService = ServiceFactory.create(ISalesmanService.class);
-		Salesman salesman = salesmanService.byEmployeeId(taskEntity.getOwnerId());
-		if (salesman != null) {
+		NCustomerTask taskEntity = (NCustomerTask)ctx.getItem();
+		
+		Integer supplierId =  taskEntity.getSupplierId();
+		Integer departId =  taskEntity.getDepartmentId();
+		Integer ownerId =  taskEntity.getOwnerId();
+		//1.根据选择的服务商、部门是否为空，判断分配状态
+		if(departId != null && supplierId == null && ownerId == null){
+			taskEntity.setAllocationState(AllocationState.ALLOCATED_Department);
+		}
+		if(supplierId != null && ownerId == null ){
+			taskEntity.setAllocationState(AllocationState.ALLOCATED_Supplier);
+		}
+		//2.服务商和部门如果不选择，此时根据业务员Id,获取相应的服务商和部门
+		if(ownerId != null){
+			taskEntity.setAllocationState(AllocationState.ALLOCATED);
+			SalesmanOrganization organization = SupplierSessionManager.getSalesmanOrganization(ownerId);
 			
-			if (taskEntity.getSupplierId() == null) {
+			if(departId == null){
 				
-				taskEntity.setSupplierId(salesman.getSupplierId());
+				taskEntity.setDepartmentId(organization.getDepartmentId());
 			}
-			
-			if (taskEntity.getDepartmentId() == null) {
+			if(supplierId == null){
 				
-				taskEntity.setDepartmentId(salesman.getDepartmentId());
+				taskEntity.setSupplierId(organization.getSupplierId());
 			}
 		}
+		
 	}
-
 }
