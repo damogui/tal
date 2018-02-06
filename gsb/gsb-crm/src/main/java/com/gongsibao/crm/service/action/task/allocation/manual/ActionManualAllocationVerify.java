1dc8207f8009d725849c7fd513f53e2cd1c8fe9f
hanwei <hwhping@163.com>
2018-02-06 15:39:45 +0800

    BUG

--- gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/task/allocation/manual/ActionManualAllocationVerify.java
+++ gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/task/allocation/manual/ActionManualAllocationVerify.java
@@ -9,23 +9,27 @@ import com.gongsibao.entity.supplier.Salesman;
 import com.gongsibao.supplier.base.ISalesmanService;
 
 /**
- * @author hw
- * 分配校验
+ * @author hw 分配校验
  */
-public class ActionManualAllocationVerify implements IAction{
+public class ActionManualAllocationVerify implements IAction {
 
 	@Override
 	public void execute(ActionContext ctx) {
-		//服务商和部门如果不选择，此时根据业务员Id,获取相应的服务商和部门
-		NCustomerTask taskEntity = (NCustomerTask)ctx.getItem();
+		
+		// 服务商和部门如果不选择，此时根据业务员Id,获取相应的服务商和部门
+		NCustomerTask taskEntity = (NCustomerTask) ctx.getItem();
 		ISalesmanService salesmanService = ServiceFactory.create(ISalesmanService.class);
-		Salesman salesman = salesmanService.byEmployeeId(taskEntity.getOwnerId());	
-		if(salesman !=null){
-			if(taskEntity.getSupplierId() == null){
+		Salesman salesman = salesmanService.byEmployeeId(taskEntity.getOwnerId());
+		if (salesman != null) {
+			
+			if (taskEntity.getSupplierId() == null) {
+				
 				taskEntity.setSupplierId(salesman.getSupplierId());
 			}
-			if(taskEntity.getDepartmentId() == null){
-				taskEntity.setDepartmentId(salesman.getSupplierId());
+			
+			if (taskEntity.getDepartmentId() == null) {
+				
+				taskEntity.setDepartmentId(salesman.getDepartmentId());
 			}
 		}
 	}
