0cda0a484e0e3f58b12363b3f23a1dccd16e79e6
yanxuebiao <xbyan@gongsibao.com>
2018-02-26 18:27:24 +0800

    crm统计分析

---
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/crm/report/CustomerServiceReportEntity.java
@@ -0,0 +1,15 @@
+package com.gongsibao.entity.crm.report;
+
+import org.netsharp.core.annotations.Table;
+import org.netsharp.entity.Persistable;
+
+
+@Table(isView=true, name = "")
+public class CustomerServiceReportEntity extends Persistable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
