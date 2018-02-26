1323fb38d9188414a5af079045d30a8643b59ac3
gj <jjguo@gongsibao.com>
2018-02-26 18:50:15 +0800

    添加相关类

--- gsb/gsb-entity/src/main/java/com/gongsibao/entity/trade/NCostReceiptMap.java
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/trade/NCostReceiptMap.java
@@ -1,7 +1,43 @@
 package com.gongsibao.entity.trade;
 
+import org.netsharp.core.annotations.Column;
+import org.netsharp.core.annotations.Table;
+import org.netsharp.entity.Entity;
+
 /**
  * Created by win on 2018/2/26.
  */
-public class NCostReceiptMap {
+@Table(name = "n_costreceipt_map", header = "消费收据关系")
+public class NCostReceiptMap  extends Entity {
+    @Column(name = "cost_id", header = "消费Id")
+    private  Integer costId;
+    @Column(name = "cost_receipt", header = "消费数据")
+    private  String costReceipt;
+
+    @Column(name = "receipt_id", header = "收据Id")
+    private  Integer receiptId;
+
+    public Integer getCostId() {
+        return costId;
+    }
+
+    public void setCostId(Integer costId) {
+        this.costId = costId;
+    }
+
+    public String getCostReceipt() {
+        return costReceipt;
+    }
+
+    public void setCostReceipt(String costReceipt) {
+        this.costReceipt = costReceipt;
+    }
+
+    public Integer getReceiptId() {
+        return receiptId;
+    }
+
+    public void setReceiptId(Integer receiptId) {
+        this.receiptId = receiptId;
+    }
 }
--- gsb/gsb-entity/src/main/java/com/gongsibao/entity/trade/NRefundTransferTargetOrderMap.java
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/trade/NRefundTransferTargetOrderMap.java
@@ -1,7 +1,31 @@
 package com.gongsibao.entity.trade;
 
+import org.netsharp.core.annotations.Column;
+import org.netsharp.entity.Entity;
+
 /**
  * Created by win on 2018/2/26.
  */
-public class NRefundTransferTargetOrderMap {
+public class NRefundTransferTargetOrderMap extends Entity {
+
+    @Column(name = "refund_id", header = "退款")
+    private Integer refundId;
+    @Column(name = "transfer_target_order_id", header = "转移目标订单")
+    private  Integer transferTargetOrderId;
+
+    public Integer getRefundId() {
+        return refundId;
+    }
+
+    public void setRefundId(Integer refundId) {
+        this.refundId = refundId;
+    }
+
+    public Integer getTransferTargetOrderId() {
+        return transferTargetOrderId;
+    }
+
+    public void setTransferTargetOrderId(Integer transferTargetOrderId) {
+        this.transferTargetOrderId = transferTargetOrderId;
+    }
 }
