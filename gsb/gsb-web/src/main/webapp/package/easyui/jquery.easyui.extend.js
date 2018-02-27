0110c328d8042ea7172b53ccba620f5180f9ac45
hanwei <hwhping@163.com>
2018-02-27 14:21:53 +0800

    去掉无用文件

--- gsb/gsb-web/src/main/webapp/package/easyui/jquery.easyui.extend.js
+++ gsb/gsb-web/src/main/webapp/package/easyui/jquery.easyui.extend.js
@@ -125,7 +125,6 @@ $.extend($.fn.validatebox.defaults.rules, {
         message: '不能含有 ,\'\"\{\}\[\] 特殊字符'
     },
     unnormal : {
-    	
     	// 验证是否包含空格和非法字符 
         validator : function(value) {
         	if(value==""){
