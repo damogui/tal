59208bfa877e82787afd881089d98ff6c66ff1b6
gj <jjguo@gongsibao.com>
2018-02-28 11:41:13 +0800

    优化接口

--- gsb/gsb-api/src/main/java/com/gongsibao/api/dto/ma/ServiceBank.java
+++ gsb/gsb-api/src/main/java/com/gongsibao/api/dto/ma/ServiceBank.java
@@ -6,27 +6,40 @@ import java.util.HashMap;
  * Created by win on 2018/2/26.
  */
 /**/
-public  class ServiceBank {
+public class ServiceBank {
 
-    public static HashMap<String,String> getBankTypes(){
-        HashMap<String,String>bankTypes=new HashMap<String, String> (){};
-        bankTypes.put ("基金","私募基金");
-        bankTypes.put ("融资租赁","融资租赁");
-        bankTypes.put ("商业保理","商业保理");
-        bankTypes.put ("典当","典当行");
-        bankTypes.put ("保险经纪","保险经纪");
-        bankTypes.put ("保险代理","保险代理");
-        bankTypes.put ("融资担保","融资担保");
-        bankTypes.put ("小额贷款","小额贷款");
-        bankTypes.put ("互联网支付","互联网支付");
-        bankTypes.put ("互联网借贷","互联网借贷");
+    public static HashMap<Integer, String> getBankTypes() {
+        HashMap<Integer, String> bankTypes = new HashMap<Integer, String> () {
+        };
+        bankTypes.put (1, "私募基金");
+        bankTypes.put (2, "融资租赁");
+        bankTypes.put (3, "商业保理");
+        bankTypes.put (4, "典当行");
+        bankTypes.put (5, "保险经纪");
+        bankTypes.put (6, "保险代理");
+        bankTypes.put (7, "融资担保");
+        bankTypes.put (8, "小额贷款");
+        bankTypes.put (9, "互联网支付");
+        bankTypes.put (10, "互联网借贷");
         return bankTypes;
-
-
     }
 
-
-
+    /*根据k返回对应的关键字*/
+    public static String getBankTypesValByKey(Object key) {
+        HashMap<Integer, String> bankTypes = new HashMap<Integer, String> () {
+        };
+        bankTypes.put (1, "基金");
+        bankTypes.put (2, "融资租赁");
+        bankTypes.put (3, "商业保理");
+        bankTypes.put (4, "典当");
+        bankTypes.put (5, "保险经纪");
+        bankTypes.put (6, "保险代理");
+        bankTypes.put (7, "融资担保");
+        bankTypes.put (8, "小额贷款");
+        bankTypes.put (9, "互联网支付");
+        bankTypes.put (10, "互联网借贷");
+        return bankTypes.get (key);
+    }
 
 
 }
--- gsb/gsb-api/src/main/java/com/gongsibao/api/service/ma/SellingDemandDTOService.java
+++ gsb/gsb-api/src/main/java/com/gongsibao/api/service/ma/SellingDemandDTOService.java
@@ -59,7 +59,7 @@ public class SellingDemandDTOService {
                 filterBuilder.append (" and company_name  like '%" + queryModel.getCompanyName () + "%'");
             }
             if (!StringManager.isNullOrEmpty (queryModel.getBankType ())) {//类金融牌照
-                filterBuilder.append (" and company_name  like '%" + queryModel.getBankType () + "%'");
+                filterBuilder.append (" and company_name  like '%" +ServiceBank.getBankTypesValByKey (queryModel.getBankType ())  + "%'");
             }
 
             if (queryModel.getComQualType () != 0) {//资质类型  qualificationDetails
@@ -372,11 +372,11 @@ public class SellingDemandDTOService {
         serviceTypeBank.setServiceType (ServiceTypeEnum.p4.getValue ());
         serviceTypeBank.setServiceName (ServiceTypeEnum.p4.getText ());
         ArrayList<ServiceDic> dicListBank = new ArrayList ();
-        HashMap<String,String> bankTypes=ServiceBank.getBankTypes ();//类金融牌照
-        for (String item:
+        HashMap<Integer,String> bankTypes=ServiceBank.getBankTypes ();//类金融牌照
+        for (Integer item:
                 bankTypes.keySet ()) {
             ServiceDic  serviceDic=new ServiceDic ();
-            serviceDic.setId (item);
+            serviceDic.setId (item.toString ());
             serviceDic.setName (bankTypes.get (item));
             dicListBank.add (serviceDic);
         }
