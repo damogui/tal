b20940016cb628e6a3df82ca257897e85e293288
hanwei <hwhping@163.com>
2018-03-05 20:37:08 +0800

    BUG

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/supplier/order/workspace/salesman/SalesmanOrderAllWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/supplier/order/workspace/salesman/SalesmanOrderAllWorkspaceTest.java
@@ -11,6 +11,7 @@ import org.netsharp.meta.base.WorkspaceCreationBase;
 import org.netsharp.organization.dic.OperationTypes;
 import org.netsharp.organization.entity.OperationType;
 import org.netsharp.panda.controls.ControlTypes;
+import org.netsharp.panda.dic.DatagridAlign;
 import org.netsharp.panda.entity.PDatagrid;
 import org.netsharp.panda.entity.PDatagridColumn;
 import org.netsharp.panda.entity.PForm;
@@ -157,23 +158,34 @@ public class SalesmanOrderAllWorkspaceTest extends WorkspaceCreationBase {
 
         PDatagridColumn column = null;
         addColumn(datagrid, "id", "操作", ControlTypes.OPERATION_COLUMN, 100, true);
-        addColumn(datagrid, "no", "订单编号", ControlTypes.TEXT_BOX, 80);
+        column = addColumn(datagrid, "no", "订单编号", ControlTypes.TEXT_BOX, 80);{
+        	column.setAlign(DatagridAlign.CENTER);
+        }
         addColumn(datagrid, "channelOrderNo", "渠道订单编号", ControlTypes.TEXT_BOX, 100);
-        addColumn(datagrid, "payTime", "回款日期", ControlTypes.DATETIME_BOX, 100);
+        addColumn(datagrid, "payTime", "回款日期", ControlTypes.DATE_BOX, 100);
         addColumn(datagrid, "addTime", "下单时间", ControlTypes.DATETIME_BOX, 100);
-        addColumn(datagrid, "prodName", "产品名称", ControlTypes.TEXT_BOX, 100);
+        addColumn(datagrid, "prodName", "产品名称", ControlTypes.TEXT_BOX, 200);
         addColumn(datagrid, "no", "办理名称", ControlTypes.TEXT_BOX, 100);
-        addColumn(datagrid, "payStatus", "订单支付状态", ControlTypes.ENUM_BOX, 100);
-        addColumn(datagrid, "no", "关联企业", ControlTypes.TEXT_BOX, 100);
-        addColumn(datagrid, "refundStatus", "退单状态", ControlTypes.ENUM_BOX, 100);
-        addColumn(datagrid, "totalPrice", "原价金额", ControlTypes.DECIMAL_FEN_BOX, 100);
-        addColumn(datagrid, "payablePrice", "应付金额", ControlTypes.DECIMAL_FEN_BOX, 100);
-        addColumn(datagrid, "paidPrice", "已付金额", ControlTypes.DECIMAL_FEN_BOX, 100);
-        addColumn(datagrid, "isInstallment", "是否分期付款", ControlTypes.BOOLCOMBO_BOX, 100);
-        addColumn(datagrid, "owner.name", "业务员", ControlTypes.TEXT_BOX, 100);
+        addColumn(datagrid, "payStatus", "支付状态", ControlTypes.ENUM_BOX, 60);
+        addColumn(datagrid, "companyIntention.name", "关联企业", ControlTypes.TEXT_BOX, 100);
+        addColumn(datagrid, "refundStatus", "退单状态", ControlTypes.ENUM_BOX, 60);
+        column = addColumn(datagrid, "totalPrice", "原价金额", ControlTypes.DECIMAL_FEN_BOX, 80);{
+        	
+        	column.setAlign(DatagridAlign.RIGHT);
+        }
+        column = addColumn(datagrid, "payablePrice", "应付金额", ControlTypes.DECIMAL_FEN_BOX, 80);{
+        	
+        	column.setAlign(DatagridAlign.RIGHT);
+        }
+        column = addColumn(datagrid, "paidPrice", "已付金额", ControlTypes.DECIMAL_FEN_BOX, 80);{
+        	
+        	column.setAlign(DatagridAlign.RIGHT);
+        }
+        addColumn(datagrid, "isInstallment", "是否分期", ControlTypes.BOOLCOMBO_BOX, 80);
+        addColumn(datagrid, "owner.name", "业务员", ControlTypes.TEXT_BOX, 80);
         addColumn(datagrid, "customerName", "下单客户", ControlTypes.TEXT_BOX, 100);
-        addColumn(datagrid, "platformSource", "订单来源", ControlTypes.ENUM_BOX, 100);
-        addColumn(datagrid, "sourceType", "下单方式", ControlTypes.ENUM_BOX, 100);
+        addColumn(datagrid, "platformSource", "订单来源", ControlTypes.ENUM_BOX, 80);
+        addColumn(datagrid, "sourceType", "下单方式", ControlTypes.ENUM_BOX, 80);
 
         return datagrid;
     }
