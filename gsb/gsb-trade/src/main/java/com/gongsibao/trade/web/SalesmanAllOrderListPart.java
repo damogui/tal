9026c8f5aaaadbc047229ee2b434428ee83b3f3a
zhangchao <chaozhang@gongsibao.com>
2018-03-06 17:20:31 +0800

    fix

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/SalesmanAllOrderListPart.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/SalesmanAllOrderListPart.java
@@ -6,18 +6,45 @@ import com.gongsibao.trade.base.IOrderProdTraceService;
 import com.gongsibao.u8.base.ISoOrderService;
 import org.netsharp.communication.ServiceFactory;
 import org.netsharp.panda.commerce.AdvancedListPart;
+import org.netsharp.panda.commerce.FilterParameter;
 import org.netsharp.panda.commerce.ListPart;
+import org.netsharp.util.StringManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by win on 2018/3/2.
  */
 public class SalesmanAllOrderListPart extends AdvancedListPart {
-    ISoOrderService prodTraceService = ServiceFactory.create(ISoOrderService.class);
+    ISoOrderService orderService = ServiceFactory.create(ISoOrderService.class);
 
 //    public List<SoOrder> querySoOrderTraceList(Integer soOrderId) {
 //        List<SoOrder> orderTraceList = prodTraceService.queryList (soOrderId);
 //        return orderTraceList;
 //    }
+
+
+    @Override
+    public String getFilterByParameter(FilterParameter parameter) {
+
+        ArrayList<String> filters = new ArrayList<String>();
+        //当是关键字时(订单编号、渠道订单编号、下单人、下单人电话、关联公司)
+        String keyword = parameter.getValue1().toString();
+        if (parameter.getKey().equals("keyword")) {
+
+            filters.add("no like '%" + keyword + "%'");
+            filters.add("channel_order_no like '%" + keyword + "%'");
+            filters.add("account_mobile like '%" + keyword + "%'");
+            filters.add("customer_name like '%" + keyword + "%'");
+            filters.add("company_id in( select pkid from crm_company_intention where (name like '%" + keyword + "%' or full_name like '%" + keyword + "%' or company_name like '%" + keyword + "%' )  )");
+            return "(" + StringManager.join(" or ", filters) + ")";
+        }
+        //业务员
+        if (parameter.getKey().equals("ywyName")) {
+            return "owner_id in (select id from sys_permission_employee where name = '" + keyword + "')";
+        }
+
+        return parameter.getFilter();
+    }
 }
