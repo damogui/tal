f19901f72aa8584378cefbbcd0f8b129632f0873
zhouruifan <rfzhou@gongsibao.com>
2018-03-07 11:42:10 +0800

    商标变更转让优化

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/igirl/workspace/DpTradeMarkFollowWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/igirl/workspace/DpTradeMarkFollowWorkspaceTest.java
@@ -127,7 +127,7 @@ public class DpTradeMarkFollowWorkspaceTest extends WorkspaceCreationBase{
 		addColumn(datagrid, "createTime", "创建时间", ControlTypes.DATETIME_BOX, 120);
 		addColumn(datagrid, "markSubmitTime", "商标提交时间", ControlTypes.DATETIME_BOX, 120);
 		addColumn(datagrid, "tradeMarkCase.ownerName", "业务人员", ControlTypes.TEXT_BOX, 80);
-		addColumn(datagrid, "nclOneId", "商标大类", ControlTypes.TEXT_BOX, 50);
+		addColumn(datagrid, "nclOne.code", "商标大类", ControlTypes.TEXT_BOX, 50);
 		addColumn(datagrid, "code", "商标号", ControlTypes.TEXT_BOX, 120);
 		addColumn(datagrid, "proxyCode", "代理号", ControlTypes.TEXT_BOX, 120);
 		addColumn(datagrid, "tradeMarkCase.companyName", "公司名称", ControlTypes.TEXT_BOX, 200);
