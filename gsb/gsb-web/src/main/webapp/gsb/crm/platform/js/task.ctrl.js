2c364cb7545b216996912547a751426276b5b319
hanwei <hwhping@163.com>
2018-03-09 19:14:36 +0800

    结构调整

--- gsb/gsb-web/src/main/webapp/gsb/crm/platform/js/task.ctrl.js
+++
@@ -1,8 +0,0 @@
-System.Declare("com.gongsibao.crm.web");
-com.gongsibao.crm.web.PlatformTaskCtrl = com.gongsibao.crm.web.TaskCtrl.Extends({
-	ctor : function() {
-		
-		this.taskUrl = '/panda/crm/platform/task/edit';
-		this.customerUrl = '/panda/crm/platform/customer/edit';
-	}
-});
