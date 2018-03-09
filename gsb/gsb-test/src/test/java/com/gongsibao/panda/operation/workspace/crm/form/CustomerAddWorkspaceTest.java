0e2695cc9704f5956760b9f159c70983f9b5f2df
hanwei <hwhping@163.com>
2018-03-09 21:24:20 +0800

    结构调整

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/operation/workspace/crm/form/CustomerAddWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/operation/workspace/crm/form/CustomerAddWorkspaceTest.java
@@ -50,7 +50,7 @@ public class CustomerAddWorkspaceTest extends WorkspaceCreationBase {
 		
 		List<String> ss = new ArrayList<String>();
 		ss.add("/gsb/supplier/crm/base/js/customer-base-form.part.js");
-		ss.add("/gsb/crm/platform/js/customer-add-form.part.js");
+		ss.add("/gsb/platform/operation/crm/js/customer-add-form.part.js");
 		ss.add("/gsb/panda-extend/gsb.customer.controls.js");
 		formJsImport = StringManager.join("|", ss);
 		formJsController = "com.gongsibao.crm.web.NCustomerPlatformAddFormPart";
