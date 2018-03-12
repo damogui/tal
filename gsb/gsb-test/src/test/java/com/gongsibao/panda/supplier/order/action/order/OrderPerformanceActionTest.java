db6aa1a945aa298a61a6c25da428adbffacbf964
hanwei <hwhping@163.com>
2018-03-12 17:18:35 +0800

    Action创建

---
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/supplier/order/action/order/OrderPerformanceActionTest.java
@@ -0,0 +1,48 @@
+package com.gongsibao.panda.supplier.order.action.order;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.netsharp.plugin.bean.BeanPath;
+
+import com.gongsibao.panda.supplier.crm.action.BaseActionTest;
+import com.gongsibao.trade.service.action.order.performance.ActionApplyOrderPerformanceAudit;
+import com.gongsibao.trade.service.action.order.performance.ActionApplyOrderPerformancePersist;
+import com.gongsibao.trade.service.action.order.performance.ActionApplyOrderPerformanceSendMessage;
+import com.gongsibao.trade.service.action.order.performance.ActionApplyOrderPerformanceVerify;
+
+/**   
+ * @ClassName:  OrderPerformanceActionTest   
+ * @Description:TODO 订单业绩
+ * @author: 韩伟
+ * @date:   2018年3月12日 下午5:02:57   
+ *     
+ * @Copyright: 2018 www.yikuaxiu.com Inc. All rights reserved. 
+ */
+public class OrderPerformanceActionTest extends BaseActionTest{
+
+	@Before
+	public void setup() {
+
+		resourceNodeCode = "Gsb_Supplier_Order_Salesman_Add";
+		super.setup();
+	}
+
+	@Test
+	public void save() {
+		
+		String pathName = "gsb/crm/order/performance";
+		BeanPath beanPath = new BeanPath();
+		{
+			beanPath.toNew();
+			beanPath.setPath(pathName);
+			beanPath.setResourceNode(resourceNode);
+			beanPath.setName("申请订单业绩");
+		}
+
+		createBean(beanPath, "1.验证", ActionApplyOrderPerformanceVerify.class.getName(), resourceNode, 100);
+		createBean(beanPath, "2.保存", ActionApplyOrderPerformancePersist.class.getName(), resourceNode, 200);
+		createBean(beanPath, "4.审核", ActionApplyOrderPerformanceAudit.class.getName(), resourceNode, 300);
+		createBean(beanPath, "5.通知", ActionApplyOrderPerformanceSendMessage.class.getName(), resourceNode, 400);
+		beanPathService.save(beanPath);
+	}
+}
