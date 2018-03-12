a15a63ee89b85a3f55ededa09d93652b9513d503
hanwei <hwhping@163.com>
2018-03-12 17:20:41 +0800

    Action

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/supplier/order/action/ActionAllTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/supplier/order/action/ActionAllTest.java
@@ -1,6 +1,11 @@
 package com.gongsibao.panda.supplier.order.action;
 
+import com.gongsibao.panda.supplier.order.action.order.ApplyInvoiceActionTest;
+import com.gongsibao.panda.supplier.order.action.order.CarryoverActionTest;
 import com.gongsibao.panda.supplier.order.action.order.OrderApplyStageActionTest;
+import com.gongsibao.panda.supplier.order.action.order.OrderPerformanceActionTest;
+import com.gongsibao.panda.supplier.order.action.order.OrderRefundActionTest;
+import com.gongsibao.panda.supplier.order.action.order.PayPerformanceActionTest;
 import com.gongsibao.panda.supplier.order.action.order.TransformActionTest;
 
 import org.junit.runner.RunWith;
@@ -13,7 +18,12 @@ import com.gongsibao.panda.supplier.order.action.order.OrderNewSaveActionTest;
 @SuiteClasses({
         OrderNewSaveActionTest.class,
         TransformActionTest.class,
-        OrderApplyStageActionTest.class
+        OrderApplyStageActionTest.class,
+        ApplyInvoiceActionTest.class,
+        CarryoverActionTest.class,
+        OrderPerformanceActionTest.class,
+        OrderRefundActionTest.class,
+        PayPerformanceActionTest.class,
 })
 
 public class ActionAllTest {
