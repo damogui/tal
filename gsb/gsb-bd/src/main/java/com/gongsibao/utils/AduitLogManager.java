72c5f98bb61bed34b9d1ebc4c73d0c29558ce77b
hanwei <hwhping@163.com>
2018-03-15 11:48:12 +0800

    重命名

---
+++ gsb/gsb-bd/src/main/java/com/gongsibao/utils/AduitLogManager.java
@@ -0,0 +1,23 @@
+package com.gongsibao.utils;
+
+import java.util.List;
+
+import com.gongsibao.entity.bd.AuditLog;
+import com.gongsibao.entity.bd.dic.AuditLogType;
+
+public class AduitLogManager {
+
+	/**   
+	 * @Title: getAuditLogList   
+	 * @Description: TODO(获取审核日志)   
+	 * @param: @param type
+	 * @param: @param formId
+	 * @param: @return      
+	 * @return: List<AuditLog>      
+	 * @throws   
+	 */
+	public static List<AuditLog> getAuditLogList(AuditLogType type,Integer formId) {
+		
+		return null;
+	}
+}
