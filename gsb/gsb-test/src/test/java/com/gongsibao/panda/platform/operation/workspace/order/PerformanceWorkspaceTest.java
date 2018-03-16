45ef4399aa4bd59c72e048ce1945ff87847e9179
zhangchao <chaozhang@gongsibao.com>
2018-03-16 16:26:42 +0800

    fix

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/platform/operation/workspace/order/PerformanceWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/platform/operation/workspace/order/PerformanceWorkspaceTest.java
@@ -1,27 +1,19 @@
 package com.gongsibao.panda.platform.operation.workspace.order;
 
+import com.gongsibao.panda.supplier.order.workspace.salesman.SalesmanOrderPerformanceWorspaceTest;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.gongsibao.panda.supplier.order.workspace.salesman.SalesmanOrderAllWorkspaceTest;
 
-public class PerformanceWorkspaceTest extends SalesmanOrderAllWorkspaceTest{
-	@Before
-	public void setup() {
+public class PerformanceWorkspaceTest extends SalesmanOrderPerformanceWorspaceTest {
+    @Before
+    public void setup() {
 
-		super.setup();
-		urlList = "/operation/order/performance/list";
-		resourceNodeCode = "Operation_Order_Performance";
-		listToolbarPath = "panda/datagrid/edit";
-	}	
-    @Test
-    public void saveListToolbar() {
-    	
+        super.setup();
+        urlList = "/operation/order/performance/list";
+        resourceNodeCode = "Operation_Order_Performance";
+        listFilter = "";
     }
-    
 
-    @Test
-    public void saveRowToolbar() {
-    	
-    }
 }
