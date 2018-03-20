6e8e597f1670a740e6aa2de7b14adf30c3414ec1
yanxuebiao <xbyan@gongsibao.com>
2018-03-20 19:02:00 +0800

    审核弹框

--- gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit-stage-list.part.js
+++ gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit-stage-list.part.js
@@ -3,9 +3,9 @@ System.Declare("com.gongsibao.trade.web");
 com.gongsibao.trade.web.AuditStageListPart = org.netsharp.panda.commerce.ListPart.Extends({
     ctor: function () {
         this.base();
-        this.auditStageUrl = '/nav/gsb/platform/trade/auditStage';//分期审核
+        this.auditStageUrl = '/nav/gsb/platform/trade/auditStage';//分期审核jsp
     },
-    auditStage: function (id) {//创建分期
+    auditStage: function (id) {//订单审核-分期审核
         var me = this;
         var row = this.getSelectedItem();
         var rows = this.getSelections();
@@ -16,7 +16,7 @@ com.gongsibao.trade.web.AuditStageListPart = org.netsharp.panda.commerce.ListPar
         var contentUrl = this.auditStageUrl + "?id=" + row.id;
         layer.open({
             type: 2,//1是字符串 2是内容
-            title: '创建分期',
+            title: '分期审核',
             fixed: false,
             maxmin: true,
             shadeClose: false,
@@ -24,12 +24,29 @@ com.gongsibao.trade.web.AuditStageListPart = org.netsharp.panda.commerce.ListPar
             zIndex: 100000,
             id: "auditStageIframe",
             content: contentUrl,
-            btn: ['保存', '取消'],// 可以无限个按钮
-            yes: function (index, layero) {
-                document.getElementById('auditStageIframe').firstElementChild.contentWindow.auditStageCtrl.auditStage();
+            btn: ['审核通过', '审核不通过'],// 可以无限个按钮
+            btn1: function (index, layero) {
+                document.getElementById('auditStageIframe').firstElementChild.contentWindow.auditStageCtrl.approved();
             },
+            btn2: function (){
+            	document.getElementById('auditStageIframe').firstElementChild.contentWindow.auditStageCtrl.rejected();
+            }
         });
 
+    },
+    detail : function(id){
+    	var row = this.getSelectedItem();
+    	var contentUrl = this.auditStageUrl + "?id=" + row.id;
+    	  layer.open({
+              type: 2,//1是字符串 2是内容
+              title: '查看',
+              fixed: false,
+              maxmin: true,
+              shadeClose: false,
+              area: ['50%', '70%'],
+              zIndex: 100000,
+              content: contentUrl
+          });
     },
     serviceNameFormatter : function(value,row,index){
     	
