04836eb65c3050f65aee39c4d9f33e0cca459f52
hanwei <hwhping@163.com>
2018-03-21 16:24:22 +0800

    分配金额弹出窗口公共抽取

--- gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/salesman-order-all-list.part.js
+++ gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/salesman-order-all-list.part.js
@@ -121,7 +121,7 @@ com.gongsibao.trade.web.SalesmanAllOrderListPart = org.netsharp.panda.commerce.L
                     fixed: false,
                     maxmin: true,
                     shadeClose: false,
-                    area: ['70%', '70%'],
+                    area: ['70%', '85%'],
                     zIndex: 100000,
                     id: "addRefundIframe",
                     content: contentUrl,
