dbcb4135233d0f5f6c372a6ed28f201b9e414fbc
hanwei <hwhping@163.com>
2018-03-21 15:14:43 +0800

    开户重构

--- gsb/gsb-bd/src/main/java/com/gongsibao/supplier/base/IFunctionModuleRoleService.java
+++ gsb/gsb-bd/src/main/java/com/gongsibao/supplier/base/IFunctionModuleRoleService.java
@@ -1,9 +1,12 @@
 package com.gongsibao.supplier.base;
 
+import java.util.List;
+
 import org.netsharp.base.IPersistableService;
 
 import com.gongsibao.entity.supplier.FunctionModuleRole;
 
 public interface IFunctionModuleRoleService extends IPersistableService<FunctionModuleRole>{
 
+	 List<FunctionModuleRole> queryList(List<Integer> moduleIdList);
 }
--- gsb/gsb-bd/src/main/java/com/gongsibao/supplier/service/FunctionModuleRoleService.java
+++ gsb/gsb-bd/src/main/java/com/gongsibao/supplier/service/FunctionModuleRoleService.java
@@ -1,7 +1,11 @@
 package com.gongsibao.supplier.service;
 
+import java.util.List;
+
 import org.netsharp.communication.Service;
+import org.netsharp.core.Oql;
 import org.netsharp.service.PersistableService;
+import org.netsharp.util.StringManager;
 
 import com.gongsibao.entity.supplier.FunctionModuleRole;
 import com.gongsibao.supplier.base.IFunctionModuleRoleService;
@@ -12,5 +16,18 @@ public class FunctionModuleRoleService extends PersistableService<FunctionModule
 	public FunctionModuleRoleService() {
 		super();
 		this.type = FunctionModuleRole.class;
+	}
+
+	@Override
+	public List<FunctionModuleRole> queryList(List<Integer> moduleIdList) {
+
+		String moduleIds = StringManager.join(",", moduleIdList);
+		Oql oql = new Oql();
+		{
+			oql.setType(FunctionModuleRole.class);
+			oql.setSelects("*");
+			oql.setFilter("function_module_id in (" + moduleIds + ")");
+		}
+		return this.queryList(oql);
 	}
 }
