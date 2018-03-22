3724880ef8fed36ff9709c3468e84e667f313944
gj <jjguo@gongsibao.com>
2018-03-22 13:46:14 +0800

    订单业绩审核

--- gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit-order-performance.js
+++ gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit-order-performance.js
@@ -1,3 +1,23 @@
 /**
  * Created by win on 2018/3/22.
  */
+System.Declare("com.gongsibao.trade.web");
+//订单业绩审核
+com.gongsibao.trade.web.AuditOrderPerformanceListPart = org.netsharp.panda.commerce.ListPart.Extends({
+    ctor: function () {
+        this.base();
+
+        // this.addOrderReceivedUrl = '/panda/crm/order/salesman/coperformance';//创建订单业绩
+        // this.addReceivedUrl = "/panda/crm/order/salesman/creceivedperformance";//回款业绩
+
+
+    },
+
+    audit: function () {
+        debugger;
+        alert("审核");
+
+    }
+
+
+});
