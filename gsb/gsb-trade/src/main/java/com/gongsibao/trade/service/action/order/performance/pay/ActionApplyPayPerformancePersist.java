59c962cce28cb088edceb86d5487659833f14fa7
hanwei <hwhping@163.com>
2018-03-22 19:01:07 +0800

    增加回款业绩Action

---
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/order/performance/pay/ActionApplyPayPerformancePersist.java
@@ -0,0 +1,20 @@
+package com.gongsibao.trade.service.action.order.performance.pay;
+
+import org.netsharp.action.ActionContext;
+import org.netsharp.action.IAction;
+
+/**
+ * @ClassName: ActionApplyPayPersist
+ * @Description:TODO 持久化
+ * @author: 韩伟
+ * @date: 2018年3月22日 下午5:50:26
+ * 
+ * @Copyright: 2018 www.yikuaxiu.com Inc. All rights reserved.
+ */
+public class ActionApplyPayPerformancePersist implements IAction {
+
+	@Override
+	public void execute(ActionContext ctx) {
+
+	}
+}
---
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/order/performance/pay/ActionApplyPayPerformanceSendMessage.java
@@ -0,0 +1,22 @@
+package com.gongsibao.trade.service.action.order.performance.pay;
+
+import org.netsharp.action.ActionContext;
+import org.netsharp.action.IAction;
+
+/**   
+ * @ClassName:  ActionApplyPerformanceSendMessage   
+ * @Description:TODO 创建回款业绩通知
+ * @author: 韩伟
+ * @date:   2018年3月22日 下午6:44:16   
+ *     
+ * @Copyright: 2018 www.yikuaxiu.com Inc. All rights reserved. 
+ */
+public class ActionApplyPayPerformanceSendMessage  implements IAction{
+
+	@Override
+	public void execute(ActionContext ctx) {
+
+		
+	}
+
+}
