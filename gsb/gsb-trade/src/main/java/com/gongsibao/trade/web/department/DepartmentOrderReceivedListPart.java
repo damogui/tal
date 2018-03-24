b2e22c2f6cac8bef9e873913de5024f36dfd5c38
hanwei <hwhping@163.com>
2018-03-24 10:49:50 +0800

    优化

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/department/DepartmentOrderReceivedListPart.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/department/DepartmentOrderReceivedListPart.java
@@ -1,9 +1,12 @@
 package com.gongsibao.trade.web.department;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.netsharp.panda.commerce.FilterParameter;
 import org.netsharp.util.StringManager;
+
+import com.gongsibao.utils.SupplierSessionManager;
 
 public class DepartmentOrderReceivedListPart extends DepartmentOrderAllListPart{
     @Override
@@ -24,4 +27,28 @@ public class DepartmentOrderReceivedListPart extends DepartmentOrderAllListPart{
 
         return parameter.getFilter();
     }
+    
+	protected String getExtraFilter() {
+
+		List<String> ss = new ArrayList<String>();
+
+		// 父类过滤条件
+		String filter = super.getExtraFilter();
+		if (!StringManager.isNullOrEmpty(filter)) {
+			ss.add(filter);
+		}
+
+		// 过滤部门Id
+		String departmentIds = SupplierSessionManager.getSubDepartmentIdsStr();
+		if (!StringManager.isNullOrEmpty(departmentIds)) {
+
+			ss.add("departmentId in (" + departmentIds + ")");
+		} else {
+
+			// 非服务商内部人员看不到数据
+			ss.add("1=2");
+		}
+
+		return StringManager.join(" and ", ss);
+	}
 }
