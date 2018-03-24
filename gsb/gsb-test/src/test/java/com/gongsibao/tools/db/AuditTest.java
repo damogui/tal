9e4c5067188b34f7628d3cbeb95e0bfaf0465995
yanxuebiao <xbyan@gongsibao.com>
2018-03-24 14:27:37 +0800

    1

--- gsb/gsb-test/src/test/java/com/gongsibao/tools/db/AuditTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/tools/db/AuditTest.java
@@ -1,14 +1,11 @@
 package com.gongsibao.tools.db;
 
-import com.gongsibao.bd.service.auditLog.AbstractAuditLogService;
-import com.gongsibao.bd.service.auditLog.AuditFactory;
-import com.gongsibao.bd.service.auditLog.SingleBackAudit;
 
 public class AuditTest {
 
 	public static void main(String[] args) {
-		AbstractAuditLogService service = AuditFactory.getAudit(SingleBackAudit.class);
-		service.execute(2, 1806);
+		/*AbstractAuditLogService service = AuditFactory.getAudit(SingleBackAudit.class);
+		service.execute(2, 1806);*/
 	}
 
 }
