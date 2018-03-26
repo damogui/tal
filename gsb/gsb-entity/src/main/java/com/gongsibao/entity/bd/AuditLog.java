e966e8fe4ebf0a5cdb56b98eea1c0e1bebf873d4
zhangchao <chaozhang@gongsibao.com>
2018-03-26 19:45:32 +0800

    fix

--- gsb/gsb-entity/src/main/java/com/gongsibao/entity/bd/AuditLog.java
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/bd/AuditLog.java
@@ -13,35 +13,7 @@ import org.netsharp.organization.entity.Employee;
 
 @Table(name = "bd_audit_log")
 public class AuditLog extends BaseEntity {
-<<<<<<< HEAD
-    /**
-     * @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
-     */
-    private static final long serialVersionUID = -8815735808036860870L;
 
-    @Column(name = "type_id", header = "审核类型序号，type=104，1041产品定价申请审核、1042订单改价申请审核、1043合同申请审核、1044发票申请审核、1045收款申请审核、1046退单申请审核、1047分期申请审核、1048产品改价申请审核、1050订单业绩审核")
-    private AuditLogType type = AuditLogType.wu;
-
-    @Column(name = "form_id", header = "关联表外键")
-    private Integer formId;
-
-    @Column(name = "status_id", header = "审核状态序号，type=105，1051 待审核、1052 审核中、1053 驳回审核、1054 审核通过、1055排队、1056关闭")
-    private AuditLogStatusType status = AuditLogStatusType.TOAUDIT;
-
-    @Column(name = "content", header = "审批内容")
-    private String content;
-
-    @Column(name = "remark", header = "说明")
-    private String remark;
-
-    @Column(name = "level", header = "审核层级")
-    private Integer level;
-
-    @Column(name = "max_level", header = "最高审核层级（用于在审核通过时，和当前级别比较下，相等时进行修改主实体状态等操作）")
-    private Integer maxLevel;
-
-    // 订单  订单业绩和回款业绩共用
-=======
 	/**
 	 * @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
 	 */
@@ -73,10 +45,6 @@ public class AuditLog extends BaseEntity {
 	private Employee employee;
 	
 	// 订单
-	@Reference(foreignKey = "formId")
-	private SoOrder soOrder;
-    // 订单业绩
->>>>>>> origin/dev/orderoption
     @Reference(foreignKey = "formId")
     private SoOrder soOrder;
 
@@ -88,7 +56,6 @@ public class AuditLog extends BaseEntity {
     @Reference(foreignKey = "formId")
     private Pay pay;
 
-
     // 合同
     @Reference(foreignKey = "formId", primaryKey = "pkid")
     private Contract contract;
@@ -96,7 +63,6 @@ public class AuditLog extends BaseEntity {
     // 发票
     @Reference(foreignKey = "formId", primaryKey = "pkid")
     private Invoice invoice;
-
 
     // 退单记录
     @Reference(foreignKey = "formId")
