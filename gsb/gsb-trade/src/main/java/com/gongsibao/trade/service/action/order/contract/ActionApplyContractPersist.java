2cfd4a38bde2c3b85d627e117350b5a4c7c14c9f
zhangchao <chaozhang@gongsibao.com>
2018-03-26 10:53:10 +0800

    fix

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/order/contract/ActionApplyContractPersist.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/order/contract/ActionApplyContractPersist.java
@@ -2,8 +2,10 @@ package com.gongsibao.trade.service.action.order.contract;
 
 import com.gongsibao.entity.crm.NCustomer;
 import com.gongsibao.entity.trade.Contract;
+import com.gongsibao.entity.trade.SoOrder;
 import com.gongsibao.entity.trade.dic.AuditStatusType;
 import com.gongsibao.trade.base.IContractService;
+import com.gongsibao.u8.base.ISoOrderService;
 import org.netsharp.action.ActionContext;
 import org.netsharp.action.IAction;
 import org.netsharp.base.IPersistableService;
@@ -17,16 +19,23 @@ public class ActionApplyContractPersist implements IAction {
 
     IContractService contractService = ServiceFactory.create(IContractService.class);
 
+    ISoOrderService soOrderService = ServiceFactory.create(ISoOrderService.class);
+
     @Override
     public void execute(ActionContext ctx) {
         //合同
         Contract contract = (Contract) ctx.getItem();
         //初始状态为【待审核】
         contract.setAuditStatusId(AuditStatusType.Dsh);
+
+        SoOrder order = soOrderService.getByOrderId(contract.getOrderId());
+        //分配合同跟进人
+        contract.setSupplierId(order.getSupplierId());
+        contract.setDepartmentId(order.getDepartmentId());
+        contract.setSalesmanId(order.getOwnerId());
         contract = contractService.save(contract);
         //更新合同
         ctx.setItem(contract);
-
 
 
     }
