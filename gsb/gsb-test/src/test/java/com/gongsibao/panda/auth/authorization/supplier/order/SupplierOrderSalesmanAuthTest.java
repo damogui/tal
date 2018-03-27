282da6375aa08e3c6a7a9c65be382cb495f69078
gj <jjguo@gongsibao.com>
2018-03-27 13:54:06 +0800

    进行预置角色授权

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/auth/authorization/supplier/order/SupplierOrderSalesmanAuthTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/auth/authorization/supplier/order/SupplierOrderSalesmanAuthTest.java
@@ -3,7 +3,7 @@ package com.gongsibao.panda.auth.authorization.supplier.order;
 import org.junit.Before;
 
 import com.gongsibao.panda.auth.authorization.AuthBaseTest;
-
+/*订单业务员授权*/
 public class SupplierOrderSalesmanAuthTest extends AuthBaseTest{
 	@Before
 	public void setup() {
