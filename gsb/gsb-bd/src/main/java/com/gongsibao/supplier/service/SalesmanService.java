1f02254ac519c064d84c2727b3f845aea5b4d042
yanxuebiao <xbyan@gongsibao.com>
2018-03-28 18:54:53 +0800

    1

--- gsb/gsb-bd/src/main/java/com/gongsibao/supplier/service/SalesmanService.java
+++ gsb/gsb-bd/src/main/java/com/gongsibao/supplier/service/SalesmanService.java
@@ -429,10 +429,10 @@ public class SalesmanService extends SupplierPersistableService<Salesman> implem
     @Override
     public List<Integer> getEmployeeIdListByRoleCodes(List<String> roleCodes) {
 
-        List<Integer> employeeIdList = new ArrayList();
+        List<Integer> employeeIdList = new ArrayList(); 
         StringBuffer sql = new StringBuffer();
 
-        if (CollectionUtils.isEmpty(roleCodes)) {
+        if (CollectionUtils.isEmpty(roleCodes)) { 
             return new ArrayList();
         }
 
