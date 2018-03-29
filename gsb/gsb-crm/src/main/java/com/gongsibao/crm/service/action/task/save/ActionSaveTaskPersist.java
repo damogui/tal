7c31f5a34a083b3b5414992d1500a6982a58f36a
hanwei <hwhping@163.com>
2018-03-29 19:33:38 +0800

    客户信息里跟进日志和意向产品应该是名下所有商机的跟进和意向产品，但目前任务添加之后，仍显示为空

--- gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/task/save/ActionSaveTaskPersist.java
+++ gsb/gsb-crm/src/main/java/com/gongsibao/crm/service/action/task/save/ActionSaveTaskPersist.java
@@ -24,84 +24,102 @@ import com.gongsibao.entity.crm.dic.NAllocationType;
  */
 public class ActionSaveTaskPersist implements IAction {
 
-    @Override
-    public void execute(ActionContext ctx) {
+	@Override
+	public void execute(ActionContext ctx) {
 
-        NCustomerTask task = (NCustomerTask) ctx.getItem();
-        EntityState state = task.getEntityState();
+		NCustomerTask task = (NCustomerTask) ctx.getItem();
+		EntityState state = task.getEntityState();
 
-        // 如果商机名称为空，则自动生成（默认取客户意向产品、意向地区，支持手动填写/修改）
-        if (StringManager.isNullOrEmpty(task.getName())) {
+		// 如果商机名称为空，则自动生成（默认取客户意向产品、意向地区，支持手动填写/修改）
+		if (StringManager.isNullOrEmpty(task.getName())) {
 
-            createTaskName(task);
-        }
+			createTaskName(task);
+		}
 
-        //新增状态下，如果是市场投放则自动代入费用部门
-        if (state == EntityState.New && task.getCosted()) {
+		// 新增状态下，如果是市场投放则自动代入费用部门
+		if (state == EntityState.New && task.getCosted()) {
 
-            task.setCostSupplierId(task.getSupplierId());
-        }
+			task.setCostSupplierId(task.getSupplierId());
+		}
 
-        NAllocationType allocationType = task.getAllocationType();
-        if (allocationType == NAllocationType.MANUAL) {
+		NAllocationType allocationType = task.getAllocationType();
+		if (allocationType == NAllocationType.MANUAL) {
 
-            // 【手动分配】 时设置分配状态为【已经分配】
-            //task.setAllocationState(AllocationState.ALLOCATED);
+			// 【手动分配】 时设置分配状态为【已经分配】
+			// task.setAllocationState(AllocationState.ALLOCATED);
 
-            if (state == EntityState.New) {
+			if (state == EntityState.New) {
 
-                // 新增状态：设置最后分配时间，最后分配人
-                task.setLastAllocationTime(new Date());
-                task.setLastAllocationUserId(SessionManager.getUserId());
-                task.setFoolowStatus(CustomerFollowStatus.UNSTART);
-            }
-        }
-        //当该商机有：【市场投放费用】时，则将该商机的市场投放服务商更新为跟进服务商
-        if (task.getCosted() && NumberUtils.toInt(task.getCostSupplierId()) == 0) {
-            task.setCostSupplierId(task.getSupplierId());
-        }
+				// 新增状态：设置最后分配时间，最后分配人
+				task.setLastAllocationTime(new Date());
+				task.setLastAllocationUserId(SessionManager.getUserId());
+				task.setFoolowStatus(CustomerFollowStatus.UNSTART);
+			}
+		}
+		// 当该商机有：【市场投放费用】时，则将该商机的市场投放服务商更新为跟进服务商
+		if (task.getCosted() && NumberUtils.toInt(task.getCostSupplierId()) == 0) {
+			task.setCostSupplierId(task.getSupplierId());
+		}
 
-        @SuppressWarnings("unchecked")
-        IPersistableService<NCustomerTask> service = (IPersistableService<NCustomerTask>) ReflectManager.newInstance(NCustomerService.class.getSuperclass());
-        task = service.save(task);
-        ctx.setItem(task);
-    }
+		this.relevanceProductCustomerId(task);
+		
+		@SuppressWarnings("unchecked")
+		IPersistableService<NCustomerTask> service = (IPersistableService<NCustomerTask>) ReflectManager.newInstance(NCustomerService.class.getSuperclass());
+		task = service.save(task);
+		ctx.setItem(task);
+	}
 
-    private void createTaskName(NCustomerTask task) {
+	private void createTaskName(NCustomerTask task) {
 
-        List<String> ss = new ArrayList<String>();
-        List<NCustomerProduct> productList = task.getProducts();
-        if (productList != null && productList.size() > 0) {
+		List<String> ss = new ArrayList<String>();
+		List<NCustomerProduct> productList = task.getProducts();
+		if (productList != null && productList.size() > 0) {
 
-            NCustomerProduct nCustomerProduct = productList.get(0);
-            if (nCustomerProduct.getProduct() != null) {
+			NCustomerProduct nCustomerProduct = productList.get(0);
+			if (nCustomerProduct.getProduct() != null) {
 
-                ss.add(nCustomerProduct.getProduct().getName());
-            }
+				ss.add(nCustomerProduct.getProduct().getName());
+			}
 
-            List<String> countyList = new ArrayList<String>();
-            if (nCustomerProduct.getProvinceId() != null && nCustomerProduct.getProvince() != null) {
+			List<String> countyList = new ArrayList<String>();
+			if (nCustomerProduct.getProvinceId() != null && nCustomerProduct.getProvince() != null) {
 
-                countyList.add(nCustomerProduct.getProvince().getName());
-            }
+				countyList.add(nCustomerProduct.getProvince().getName());
+			}
 
-            if (nCustomerProduct.getCityId() != null && nCustomerProduct.getCity() != null) {
+			if (nCustomerProduct.getCityId() != null && nCustomerProduct.getCity() != null) {
 
-                countyList.add(nCustomerProduct.getCity().getName());
-            }
+				countyList.add(nCustomerProduct.getCity().getName());
+			}
 
-            if (nCustomerProduct.getCountyId() != null && nCustomerProduct.getCounty() != null) {
+			if (nCustomerProduct.getCountyId() != null && nCustomerProduct.getCounty() != null) {
 
-                countyList.add(nCustomerProduct.getCounty().getName());
-            }
+				countyList.add(nCustomerProduct.getCounty().getName());
+			}
 
-            if (countyList.size() > 0) {
+			if (countyList.size() > 0) {
 
-                String countyName = "(" + StringManager.join(",", countyList) + ")";
-                ss.add(countyName);
-            }
-            String name = StringManager.join(" - ", ss);
-            task.setName(name);
-        }
-    }
+				String countyName = "(" + StringManager.join(",", countyList) + ")";
+				ss.add(countyName);
+			}
+			String name = StringManager.join(" - ", ss);
+			task.setName(name);
+		}
+	}
+
+	/**   
+	 * @Title: relevanceProductCustomerId   
+	 * @Description: TODO(关联意向产品的customerId)   
+	 * @param: @param task      
+	 * @return: void      
+	 * @throws   
+	 */
+	private void relevanceProductCustomerId(NCustomerTask task) {
+
+		List<NCustomerProduct> productList = task.getProducts();
+		for (NCustomerProduct product : productList) {
+
+			product.setCustomerId(task.getCustomerId());
+		}
+	}
 }
