7c1cf85146af9d59a5d8b716e1fb0fccc529c424
gj <jjguo@gongsibao.com>
2018-03-29 17:32:29 +0800

    权限梳理

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/platform/trade/workspace/audit/PerformanceAuditWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/platform/trade/workspace/audit/PerformanceAuditWorkspaceTest.java
@@ -13,19 +13,18 @@ import org.netsharp.panda.entity.PQueryProject;
 import org.netsharp.resourcenode.entity.ResourceNode;
 
 import com.gongsibao.entity.bd.AuditLog;
+
 /*交易中心订单业绩审核*/
-public class PerformanceAuditWorkspaceTest  extends WorkspaceCreationBase {
-	@Before
-	public void setup() {
-//WorkspaceCreationBase
-        //AuditOrderPerformanceWorkspaceTest
-		//entity = AuditLog.class;// 实体
-		urlList = "/trade/audit/performance/list";// 列表的url
-		//urlForm = "/trade/audit/performance/form";// 弹出框的url
-		listPartName = formPartName = "交易中心订单业绩审核";
-		meta = MtableManager.getMtable(entity);// 获取实体元数据
-		formPartName = listPartName = meta.getName();
-		resourceNodeCode = "GSB_Trade_Audit_Performance";// 菜单节点码（名称）
+public class PerformanceAuditWorkspaceTest extends AuditOrderPerformanceWorkspaceTest {
+    @Before
+    public void setup() {
+        entity = AuditLog.class;// 实体
+        urlList = "/trade/audit/performance/list";// 列表的url
+        //urlForm = "/trade/audit/performance/form";// 弹出框的url
+        listPartName = formPartName = "交易中心订单业绩审核";
+        meta = MtableManager.getMtable (entity);// 获取实体元数据
+        formPartName = listPartName = meta.getName ();
+        resourceNodeCode = "GSB_Trade_Audit_Performance";// 菜单节点码（名称）
 //
 //		formOpenMode = OpenMode.WINDOW;// 编辑框打开的形式
 //		openWindowHeight = 400;
@@ -33,11 +32,10 @@ public class PerformanceAuditWorkspaceTest  extends WorkspaceCreationBase {
 //		listFilter = "type_id=1042 AND add_user_id='{userId}' ";
 
 		/*listPartServiceController = OrderOperationController.class.getName();
-		listPartJsController = OrderOperationController.class.getName();
+        listPartJsController = OrderOperationController.class.getName();
 		listPartImportJs = "/gsb/platform/trade/js/orderoperation.list.part.js";
 		listToolbarPath = "/trade/manage/order/operation/toolbar";*/
-	}
-
+    }
 
 
 }
