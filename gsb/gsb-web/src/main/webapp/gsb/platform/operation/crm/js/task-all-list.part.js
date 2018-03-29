d448019176175d40c4074e716ad8825bfd5464eb
hanwei <hwhping@163.com>
2018-03-29 19:06:14 +0800

    BUG处理

--- gsb/gsb-web/src/main/webapp/gsb/platform/operation/crm/js/task-all-list.part.js
+++ gsb/gsb-web/src/main/webapp/gsb/platform/operation/crm/js/task-all-list.part.js
@@ -2,7 +2,7 @@ com.gongsibao.crm.web.TaskAllListPart = com.gongsibao.crm.web.BaseTaskListPart.E
 	ctor : function() {
 		this.base();
 		this.addUrl = "/panda/crm/platform/task/add";
-		this.editUrl = "/panda/crm/platform/task/edit";
+		this.editUrl = "/nav/gsb/platform/operation/crm/task";
 		this.followUrl = '/panda/crm/platform/task/follow/from';
 		this.addCustomerUrl = '/panda/crm/platform/customer/add';
 	}
---
+++ gsb/gsb-web/src/main/webapp/gsb/supplier/crm/department/task.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="UTF-8"%>
+<jsp:include page="/gsb/supplier/crm/base/task.jsp"></jsp:include>
+<script src='/gsb/supplier/crm/department/js/task.ctrl.js'></script>
+<script>
+	$(function(){
+
+		new com.gongsibao.crm.web.DepartmentTaskCtrl().init();
+	});
+</script>
