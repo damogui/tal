ff077c663145a856a768761823cb3979258ac127
gj <jjguo@gongsibao.com>
2018-03-30 16:20:48 +0800

    seo

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/audit/AuditBaseController.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/audit/AuditBaseController.java
@@ -40,8 +40,7 @@ public abstract class AuditBaseController {
 		Oql oql = new Oql();
 		{
 			oql.setType(AuditLog.class);
-			oql.setSelects("auditLog.*,");
-            oql.setSelects("employee.name");
+			oql.setSelects("auditLog.*,auditLog.employee.name");
 			oql.setFilter("formId=? and type=?");
 			oql.getParameters().add("formId", id, Types.INTEGER);
 			oql.getParameters().add("type", auditLogType, Types.INTEGER);
