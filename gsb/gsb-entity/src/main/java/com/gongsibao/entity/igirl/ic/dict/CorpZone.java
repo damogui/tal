25946c8d4e87082ad4120f3651e8b810f857074d
Yuxi <2687427560@qq.com>
2018-03-30 18:12:19 +0800

    工商注册，基础枚举

---
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/igirl/ic/dict/CorpZone.java
@@ -0,0 +1,60 @@
+package com.gongsibao.entity.igirl.ic.dict;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.netsharp.base.IEnum;
+
+/**
+ * 住所（经营场所）,生产经营地
+ * @author cyx
+ *
+ */
+public enum CorpZone implements IEnum {
+
+	DONGCHENG(0, "东城区"),
+	XICHENG(1, "西城区"),
+	CHANGYANG(2, "朝阳区"),
+	HAIDIAN(3, "海淀区"),
+	FENGTAI(4, "丰台区"),
+	SHIJINGSHAN(5, "石景山区"),
+	TONGZHOU(6, "通州区"),
+	DAXING(7, "大兴区"),
+	SHUNYI(8, "顺义区"),
+	CHANGPING(9, "昌平区"),
+	FANGSHAN(10, "房山区"),
+	MENTOUGOU(11, "门头沟区"),
+	PINGGU(12, "平谷区"),
+	HUAIROU(13, "怀柔区"),
+	MIYUN(14, "密云区"),
+	YANQING(15, "延庆区"),
+	KAIFAQU(16, "北京经济技术开发区"),
+	;
+	private int value;
+	private String text;
+
+	CorpZone(int value, String text) {
+		this.value = value;
+		this.text = text;
+	}
+
+	@JsonCreator
+	public static CorpZone getItem(int value) {
+
+		for (CorpZone item : values()) {
+
+			if (item.getValue() == value) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	@Override
+	public Integer getValue() {
+
+		return this.value;
+	}
+}
