7793c67931ebd04674bb007b668b5d648de39906
yanxuebiao <xbyan@gongsibao.com>
2018-04-02 15:07:06 +0800

    订单审核

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/audit/AuditOrderController.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/audit/AuditOrderController.java
@@ -6,10 +6,14 @@ import com.gongsibao.bd.service.auditLog.AuditState;
 import com.gongsibao.bd.service.auditLog.ChangeOrderPriceAudit;
 import com.gongsibao.entity.bd.AuditLog;
 import com.gongsibao.entity.bd.dic.AuditLogType;
+import com.gongsibao.entity.trade.OrderProd;
+import com.gongsibao.trade.base.IOrderProdService;
 import com.gongsibao.trade.web.dto.AuditLogDTO;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.netsharp.communication.ServiceFactory;
 
 public class AuditOrderController extends AuditBaseController{
 
@@ -33,7 +37,19 @@ public class AuditOrderController extends AuditBaseController{
 	public Boolean rejected(Integer auditLogId, String remark) {
         return auditLogService.audit(AuditState.NOTPASS, auditLogId, remark);
 	}
+	/**
+	 * @Title: queryProductList
+	 * @Description: TODO(查询产品明细)
+	 * @param: @param orderId
+	 * @param: @return
+	 * @return: List<OrderProd>
+	 * @throws
+	 */
+	public List<OrderProd> queryProductList(Integer orderId) {
 
+		IOrderProdService prodService = ServiceFactory.create(IOrderProdService.class);
+		return prodService.queryByOrderId(orderId);
+	}
     /*订单业绩审核流程*/
     public List<AuditLogDTO> getAuditLogList(Integer id) {
         List<AuditLog> logList = new ArrayList<AuditLog> ();
