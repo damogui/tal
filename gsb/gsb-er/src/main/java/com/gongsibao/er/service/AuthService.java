0fc2938754eaa7a199f3f6c841195cae5ea0cecf
changxian <xxchang@gongsibao.com>
2018-04-02 16:12:23 +0800

    test

--- gsb/gsb-er/src/main/java/com/gongsibao/er/service/AuthService.java
+++ gsb/gsb-er/src/main/java/com/gongsibao/er/service/AuthService.java
@@ -9,7 +9,7 @@ import com.gongsibao.er.base.IAuthService;
 @Service
 public class AuthService extends PersistableService<Auth> implements IAuthService {
 
-    public AuthService(){
+    public AuthService(){ 
         super();
         this.type=Auth.class;
     }
