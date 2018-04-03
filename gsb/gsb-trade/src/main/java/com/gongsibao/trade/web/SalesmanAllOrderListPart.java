bbadfc269e068a31d998c8cc5879a314ff2f6bbe
gj <jjguo@gongsibao.com>
2018-04-03 11:39:03 +0800

    seo

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/SalesmanAllOrderListPart.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/SalesmanAllOrderListPart.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.gongsibao.entity.trade.dto.DepPayMapDTO;
+import org.netsharp.base.IPersistableService;
 import org.netsharp.communication.ServiceFactory;
 import org.netsharp.core.Oql;
 import org.netsharp.panda.commerce.AdvancedListPart;
@@ -18,53 +19,67 @@ import com.gongsibao.u8.base.ISoOrderService;
  * Created by win on 2018/3/2.
  */
 public class SalesmanAllOrderListPart extends AdvancedListPart {
-    ISoOrderService orderService = ServiceFactory.create(ISoOrderService.class);
+    ISoOrderService orderService = ServiceFactory.create (ISoOrderService.class);
+
+
+    @Override
+    public List<?> doQuery(Oql oql) {
+
+        StringBuilder sb = new StringBuilder ();
+        sb.append ("SoOrder.*,");
+        sb.append ("SoOrder.companyIntention.name,");
+        sb.append ("SoOrder.customer.realName");
+        oql.setSelects (sb.toString ());
+        List<?> rows = orderService.queryList (oql);
+        return rows;
+    }
+
 
     @Override
     public String getFilterByParameter(FilterParameter parameter) {
 
-        ArrayList<String> filters = new ArrayList<String>();
+        ArrayList<String> filters = new ArrayList<String> ();
         //当是关键字时(订单编号、渠道订单编号、下单人、下单人电话、关联公司)
-        String keyword = parameter.getValue1().toString();
-        if (parameter.getKey().equals("keyword")) {
+        String keyword = parameter.getValue1 ().toString ();
+        if (parameter.getKey ().equals ("keyword")) {
 
-            filters.add("no like '%" + keyword + "%'");
-            filters.add("channel_order_no like '%" + keyword + "%'");
-            filters.add("account_mobile like '%" + keyword + "%'");
-            filters.add("customer_name like '%" + keyword + "%'");
-            filters.add("company_id in( select pkid from crm_company_intention where (name like '%" + keyword + "%' or full_name like '%" + keyword + "%' or company_name like '%" + keyword + "%' )  )");
-            return "(" + StringManager.join(" or ", filters) + ")";
+            filters.add ("no like '%" + keyword + "%'");
+            filters.add ("channel_order_no like '%" + keyword + "%'");
+            filters.add ("account_mobile like '%" + keyword + "%'");
+            filters.add ("customer_name like '%" + keyword + "%'");
+            filters.add ("company_id in( select pkid from crm_company_intention where (name like '%" + keyword + "%' or full_name like '%" + keyword + "%' or company_name like '%" + keyword + "%' )  )");
+            return "(" + StringManager.join (" or ", filters) + ")";
         }
         //业务员
-        if (parameter.getKey().equals("ywyName")) {
+        if (parameter.getKey ().equals ("ywyName")) {
             return "owner_id in (select id from sys_permission_employee where name = '" + keyword + "')";
         }
 
         //分期申请时间
-        if (parameter.getKey().equals("stageCreateTime")) {
+        if (parameter.getKey ().equals ("stageCreateTime")) {
 
-            List<String> stageCreateTime = new ArrayList<>();
-            if (parameter.getValue1() != null) {
-                stageCreateTime.add(" create_time >= '" + parameter.getValue1().toString() + "'");
+            List<String> stageCreateTime = new ArrayList<> ();
+            if (parameter.getValue1 () != null) {
+                stageCreateTime.add (" create_time >= '" + parameter.getValue1 ().toString () + "'");
             }
-            if (parameter.getValue2() != null) {
-                stageCreateTime.add(" create_time <= '" + parameter.getValue2().toString() + "'");
+            if (parameter.getValue2 () != null) {
+                stageCreateTime.add (" create_time <= '" + parameter.getValue2 ().toString () + "'");
             }
 
-            return "pkid in (select order_id from so_order_stage where " + StringManager.join(" and ", stageCreateTime) + " )";
+            return "pkid in (select order_id from so_order_stage where " + StringManager.join (" and ", stageCreateTime) + " )";
         }
 
         //分期申请人
-        if (parameter.getKey().equals("stageCreator")) {
+        if (parameter.getKey ().equals ("stageCreator")) {
             return "pkid in (select order_id from so_order_stage where creator like '%" + keyword + "%')";
         }
 
-        return parameter.getFilter();
+        return parameter.getFilter ();
     }
 
     //转移/分配（包括批量转移/分配）
     public void orderTran(List<Integer> orderList, Integer toUserId) {
-        orderService.orderTran(orderList, toUserId);
+        orderService.orderTran (orderList, toUserId);
     }
 
 
@@ -74,48 +89,49 @@ public class SalesmanAllOrderListPart extends AdvancedListPart {
     * @return
     */
     public Boolean isStaged(Integer id) {
-        Oql oql = new Oql();
+        Oql oql = new Oql ();
         {
-            oql.setType(SoOrder.class);
-            oql.setSelects("*");
-            oql.setFilter("id=?");
-            oql.getParameters().add("id", id, Types.INTEGER);
+            oql.setType (SoOrder.class);
+            oql.setSelects ("*");
+            oql.setFilter ("id=?");
+            oql.getParameters ().add ("id", id, Types.INTEGER);
         }
-        SoOrder entity = orderService.queryFirst(oql);
-        return entity.getIsInstallment() == null ? false : entity.getIsInstallment();
+        SoOrder entity = orderService.queryFirst (oql);
+        return entity.getIsInstallment () == null ? false : entity.getIsInstallment ();
     }
 
     /**
-	 *  获取订单的退款(结转)状态、退款(结转)金额判断
-	 * @param id 订单Id
-	 * @param type 0-退款、1-结转
-	 * @return -1 金额不足
-	 */
-	public Integer refundCarryValidate(Integer id , Integer type){
-		Integer reusltValue = 0;
-		Oql oql = new Oql();
-		{
-			oql.setType(SoOrder.class);
-			oql.setSelects("*");
-			oql.setFilter("id=?");
-			oql.getParameters().add("id", id, Types.INTEGER);
-		}
-		SoOrder entity = orderService.queryFirst(oql);
-		//退款金额
-		Integer refundPrice = entity.getRefundPrice() == null ? 0 : entity.getRefundPrice().intValue();
-		//结转金额
-		Integer carryAmount = entity.getCarryAmount() == null ? 0 : entity.getCarryAmount().intValue();
-		Integer allAmount = refundPrice.intValue() + carryAmount.intValue();
-		if((entity.getPaidPrice().intValue() - allAmount.intValue()) > 0){
-			if(type.equals(0)){
-				reusltValue = entity.getRefundStatus() == null ? 0 : entity.getRefundStatus().getValue();
-			}else {
-				reusltValue = entity.getCarryStatus() == null ? 0 : entity.getCarryStatus().getValue();
-			}
-		}else {
-			reusltValue = -1;
-		}
-		return reusltValue;
-	}
+     * 获取订单的退款(结转)状态、退款(结转)金额判断
+     *
+     * @param id   订单Id
+     * @param type 0-退款、1-结转
+     * @return -1 金额不足
+     */
+    public Integer refundCarryValidate(Integer id, Integer type) {
+        Integer reusltValue = 0;
+        Oql oql = new Oql ();
+        {
+            oql.setType (SoOrder.class);
+            oql.setSelects ("*");
+            oql.setFilter ("id=?");
+            oql.getParameters ().add ("id", id, Types.INTEGER);
+        }
+        SoOrder entity = orderService.queryFirst (oql);
+        //退款金额
+        Integer refundPrice = entity.getRefundPrice () == null ? 0 : entity.getRefundPrice ().intValue ();
+        //结转金额
+        Integer carryAmount = entity.getCarryAmount () == null ? 0 : entity.getCarryAmount ().intValue ();
+        Integer allAmount = refundPrice.intValue () + carryAmount.intValue ();
+        if ((entity.getPaidPrice ().intValue () - allAmount.intValue ()) > 0) {
+            if (type.equals (0)) {
+                reusltValue = entity.getRefundStatus () == null ? 0 : entity.getRefundStatus ().getValue ();
+            } else {
+                reusltValue = entity.getCarryStatus () == null ? 0 : entity.getCarryStatus ().getValue ();
+            }
+        } else {
+            reusltValue = -1;
+        }
+        return reusltValue;
+    }
 
 }
