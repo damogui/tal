ab802aff43eb025ef023b9e26e858cb103ff3969
gj <jjguo@gongsibao.com>
2018-04-08 20:05:22 +0800

    seo

--- gsb/gsb-bd/src/main/java/com/gongsibao/utils/NumberUtils.java
+++ gsb/gsb-bd/src/main/java/com/gongsibao/utils/NumberUtils.java
@@ -12,7 +12,7 @@ import static org.apache.commons.lang.math.NumberUtils.toDouble;
  * Created by zhangchao on 2018/2/6.
  */
 public class NumberUtils {
-    private static Format FORMAT = new DecimalFormat("#.##");
+    private static Format FORMAT = new DecimalFormat ("#.##");
 
     /**
      * 数字格式化 #.##，
@@ -21,7 +21,7 @@ public class NumberUtils {
      * @return
      */
     public static String numberFormat(Number number) {
-        return numberFormat(number, null);
+        return numberFormat (number, null);
     }
 
     /**
@@ -33,18 +33,18 @@ public class NumberUtils {
      */
     public static String numberFormat(Number number, String pattern) {
         try {
-            if (StringManager.isNullOrEmpty(pattern)) {
-                return FORMAT.format(number);
+            if (StringManager.isNullOrEmpty (pattern)) {
+                return FORMAT.format (number);
             }
-            return FORMAT.format(pattern);
+            return FORMAT.format (pattern);
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace ();
             return "";
         }
     }
 
     public static int toInt(Object object) {
-        return toInt(object, 0);
+        return toInt (object, 0);
     }
 
     public static int toInt(Object str, int defaultValue) {
@@ -52,7 +52,7 @@ public class NumberUtils {
             return defaultValue;
         } else {
             try {
-                return Integer.parseInt(StringUtils.trimToEmpty(str.toString()));
+                return Integer.parseInt (StringUtils.trimToEmpty (str.toString ()));
             } catch (NumberFormatException var3) {
                 return defaultValue;
             }
@@ -60,7 +60,7 @@ public class NumberUtils {
     }
 
     public static long toLong(Object object) {
-        return toLong(object, 0);
+        return toLong (object, 0);
     }
 
     public static long toLong(Object object, long defaultValue) {
@@ -68,7 +68,7 @@ public class NumberUtils {
             return defaultValue;
         }
         try {
-            return Long.parseLong(StringUtils.trimToEmpty(object.toString()));
+            return Long.parseLong (StringUtils.trimToEmpty (object.toString ()));
         } catch (NumberFormatException var3) {
             return defaultValue;
         }
@@ -78,22 +78,24 @@ public class NumberUtils {
         if (d == 0) {
             return 0;
         }
-        return (int) AmountUtils.round(d, 0);
+        return (int) AmountUtils.round (d, 0);
     }
 
     public static long doubleRoundLong(double d) {
         if (d == 0) {
             return 0;
         }
-        return (long) AmountUtils.round(d, 0);
+        return (long) AmountUtils.round (d, 0);
     }
 
-    /*进行除以100的操作*/
-    public static Double getRealMoney(Integer money) {
-        double m = toDouble(String.valueOf(money));
-        if (m == 0) {
-            return m;
+    /*进行除以100的操作并且两位小数*/
+    public static String getRealMoney(Integer money) {
+        Double m = (money / 100d);
+        if (money == 0) {
+            return "0";
         }
-        return m / 100d;
+        DecimalFormat df = new DecimalFormat ("0.00");//######0.00
+        String st = df.format (m);
+        return st;
     }
 }
