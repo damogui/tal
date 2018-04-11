e0e1897572924d4fb7af48626ec1a407314a928a
penghongbao <penghongbao@liangyibang.com>
2018-04-11 10:05:08 +0800

    api接口版本控制实现 微信资源文件添加

--- gsb/gsb-rest/src/main/java/com/gongsibao/rest/AppApplication.java
+++ gsb/gsb-rest/src/main/java/com/gongsibao/rest/AppApplication.java
@@ -3,9 +3,11 @@ package com.gongsibao.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ServletComponentScan("com.gongsibao.rest")
+@PropertySource("classpath:conf/weixin.properties")
 public class AppApplication {
 
 	public static void main(String[] args) {
---
+++ gsb/gsb-rest/src/main/java/com/gongsibao/rest/demo/v2/ApiSeController.java
@@ -0,0 +1,32 @@
+package com.gongsibao.rest.demo.v2;
+
+import com.gongsibao.rest.common.apiversion.Api;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wx/{v}/user")
+@Api(5)
+public class ApiSeController {
+    @Value("${wechat.appid}")
+    private String appId;
+
+    @Value("${server.port}")
+    private String port;
+    @RequestMapping(value="/hello",method=RequestMethod.GET)
+    public String helloV1() throws Exception {
+        return port+"find data v5"+appId;
+    }
+
+    @RequestMapping(value="/hello2",method=RequestMethod.GET)
+    public String helloV2() throws Exception {
+        return"find data v5 hello2";
+    }
+
+    @RequestMapping(value="/hello3",method= RequestMethod.GET)
+    public String helloV3() throws Exception {
+        return"find data v3";
+    }
+}
