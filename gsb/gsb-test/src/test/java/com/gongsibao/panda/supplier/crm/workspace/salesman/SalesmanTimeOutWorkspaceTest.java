0565619c04f5a2bf206d37a5a8db4aaa0e36b5f8
zhangchao <chaozhang@gongsibao.com>
2018-04-12 18:24:26 +0800

    fix

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/supplier/crm/workspace/salesman/SalesmanTimeOutWorkspaceTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/supplier/crm/workspace/salesman/SalesmanTimeOutWorkspaceTest.java
@@ -3,30 +3,30 @@ package com.gongsibao.panda.supplier.crm.workspace.salesman;
 import org.junit.Before;
 import org.netsharp.panda.plugin.entity.PToolbar;
 
-public class SalesmanTimeOutWorkspaceTest extends SalesmanAllTaskWorkspaceTest{
+public class SalesmanTimeOutWorkspaceTest extends SalesmanAllTaskWorkspaceTest {
 
-	@Override
-	@Before
-	public void setup() {
-		
-		super.setup();
-		
-		listPartName = "超时提醒";
-		urlList = "/crm/salesman/task/timeout/list";
-		resourceNodeCode = "CRM_SALESMAN_TIMEOUT";
-		listFilter = "foolowStatus = 3 and ownerId = '{userId}' and NOW()>nextFoolowTime";
-	}
-	
-	
-	@Override
-	public PToolbar createListToolbar() {
-		
-		return null;
-	}
-	
-	@Override
-	public PToolbar createRowToolbar() {
-		
-		return null;
-	}
+    @Override
+    @Before
+    public void setup() {
+
+        super.setup();
+
+        listPartName = "超时提醒";
+        urlList = "/crm/salesman/task/timeout/list";
+        resourceNodeCode = "CRM_SALESMAN_TIMEOUT";
+        listFilter = "foolowStatus = 3 and ownerId = '{userId}' and DATEDIFF(NOW(),next_foolow_time)>0";
+    }
+
+
+    @Override
+    public PToolbar createListToolbar() {
+
+        return null;
+    }
+
+    @Override
+    public PToolbar createRowToolbar() {
+
+        return null;
+    }
 }
