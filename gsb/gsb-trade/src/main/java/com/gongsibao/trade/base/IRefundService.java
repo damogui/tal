3863db3de98290e3ed1ba56902700e97ee2653db
yanxuebiao <xbyan@gongsibao.com>
2018-04-12 20:34:47 +0800

    bug

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/base/IRefundService.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/base/IRefundService.java
@@ -9,4 +9,10 @@ import com.gongsibao.entity.trade.Refund;
 public interface IRefundService extends IPersistableService<Refund> {
 	
 	List<Refund> queryByOrderId(Integer orderId);
+	/**
+	 * 根据退款主键获取退款实体
+	 * @param id
+	 * @return
+	 */
+	Refund queryById(Integer id);
 }
