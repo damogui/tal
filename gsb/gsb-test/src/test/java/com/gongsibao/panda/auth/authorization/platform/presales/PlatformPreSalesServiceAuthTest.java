bfdb70c110d73a11996dea17adb8c2f86e7b177c
gj <jjguo@gongsibao.com>
2018-04-14 14:57:26 +0800

    seo

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/auth/authorization/platform/presales/PlatformPreSalesServiceAuthTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/auth/authorization/platform/presales/PlatformPreSalesServiceAuthTest.java
@@ -17,7 +17,7 @@ public class PlatformPreSalesServiceAuthTest extends AuthBaseTest {
     protected void getResourceCodeList() {
 //客户管理、订单管理（全部订单）、统计分析（客服统计）
         this.resourceNodeCodeList.add ("Operation_CRM");//客户管理
-        this.resourceNodeCodeList.add ("GSB_Trade_Manage_All_Order");//订单管理(全部订单)
+        this.resourceNodeCodeList.add ("Operation_Order_All");//订单管理(全部订单)
         this.resourceNodeCodeList.add ("Operation_CRM_STATISTICAL_CustomerService");//客服统计（好像还没人做）
 
     }
