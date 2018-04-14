a222be9574425ca3a5b61594dca5156ce4e27a41
yanxuebiao <xbyan@gongsibao.com>
2018-04-14 14:50:09 +0800

    bug

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/base/IRefundItemService.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/base/IRefundItemService.java
@@ -1,8 +1,12 @@
 package com.gongsibao.trade.base;
 
+import java.util.List;
+
 import org.netsharp.base.IPersistableService;
 
 import com.gongsibao.entity.trade.RefundItem;
 
 public interface IRefundItemService extends IPersistableService<RefundItem> {
+	
+	public List<RefundItem> queryByRefundId(Integer refundId);
 }
