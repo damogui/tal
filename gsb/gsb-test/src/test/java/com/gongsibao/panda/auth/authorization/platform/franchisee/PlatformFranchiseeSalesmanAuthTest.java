8bdab3b3495991bc41b28926203b0e59bcf8916c
gj <jjguo@gongsibao.com>
2018-04-16 16:04:12 +0800

    添加招商的权限，进行预置

--- gsb/gsb-test/src/test/java/com/gongsibao/panda/auth/authorization/platform/franchisee/PlatformFranchiseeSalesmanAuthTest.java
+++ gsb/gsb-test/src/test/java/com/gongsibao/panda/auth/authorization/platform/franchisee/PlatformFranchiseeSalesmanAuthTest.java
@@ -3,12 +3,17 @@ package com.gongsibao.panda.auth.authorization.platform.franchisee;
 import org.junit.Before;
 
 import com.gongsibao.panda.auth.authorization.AuthBaseTest;
-
+/*招商_业务员（招商角色）*/
 public class PlatformFranchiseeSalesmanAuthTest extends AuthBaseTest{
 	@Before
 	public void setup() {
         roleCode = "Platform_Franchisee_Salesman";
 		super.setup();
+
+	}
+
+	protected void getResourceCodeList() {
+		this.resourceNodeCodeList.add("GSB_BD_MY");//客户管理
 
 	}
 }
