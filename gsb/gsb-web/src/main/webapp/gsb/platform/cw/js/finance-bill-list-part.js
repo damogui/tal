f666584a284c6f5e0d861de3f983a8b8ef864d49
angang <angang@gongsibao.com>
2018-04-18 16:20:24 +0800

    借款单调整

--- gsb/gsb-web/src/main/webapp/gsb/platform/cw/js/finance-bill-list-part.js
+++ gsb/gsb-web/src/main/webapp/gsb/platform/cw/js/finance-bill-list-part.js
@@ -2,7 +2,9 @@ System.Declare("com.gongsibao.cw.web");
 com.gongsibao.cw.web.FinanceBillListPart = org.netsharp.panda.commerce.ListPart.Extends({
     ctor: function () {
     	this.base();
-    	this.auditUrl = '/nav/gsb/platform/cw/audit_bill_form';
+    	this.loanAuditUrl = '/nav/gsb/platform/cw/loan_audit_bill_form';
+   	    this.exppenseAuditUrl = '/nav/gsb/platform/cw/expense_audit_bill_form';
+   	    this.paymentAuditUrl = "/nav/gsb/platform/cw/payment_audit_bill_form";
     	this.formType = PandaHelper.Enum.get('com.gongsibao.entity.cw.dict.FinanceDict$FormType');
     },
     operationFormatter:function (value,row,index){ //操作格式化
@@ -15,7 +17,20 @@ com.gongsibao.cw.web.FinanceBillListPart = org.netsharp.panda.commerce.ListPart.
     	return this.formType[value];
     },
     showAudit:function (formId,formType){ //打开审核页面
-    	var contentUrl = this.auditUrl +"?formId="+formId+"&formType="+formType;
+    	var contentUrl ;
+    	var title = "";
+    	if(formType == 3){
+    		title = "借款单";
+    		contentUrl = this.loanAuditUrl +"?formId="+formId +"&formType="+formType;
+    	}
+    	if(formType == 4){
+    		title = "报销单";
+    		contentUrl = this.exppenseAuditUrl +"?formId="+formId +"&formType="+formType;
+    	}
+    	if(formType == 5){
+    		title = "付款单";
+    		contentUrl = this.paymentAuditUrl +"?formId="+formId +"&formType="+formType;
+    	}
     	layer.open({
     		id: "financeBillIframe",
             type: 2,
@@ -23,7 +38,7 @@ com.gongsibao.cw.web.FinanceBillListPart = org.netsharp.panda.commerce.ListPart.
             fixed: false,
             maxmin: true,
             shadeClose:true,
-            area: ['60%','90%'],
+            area: ['90%','90%'],
             content: contentUrl,
             btn : [ '提交', '取消' ],
             success: function (layero, index) {
