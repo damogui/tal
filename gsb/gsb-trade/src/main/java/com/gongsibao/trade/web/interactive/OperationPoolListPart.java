a0f4c947a1628930a52767be3952209d8a1ce579
zhangchao <chaozhang@gongsibao.com>
2018-04-18 14:38:37 +0800

    fix

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/interactive/OperationPoolListPart.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/web/interactive/OperationPoolListPart.java
@@ -4,8 +4,4 @@ import org.netsharp.panda.commerce.AdvancedListPart;
 
 public class OperationPoolListPart extends MyInChargeListPart {
 
-    //批量转移操作员
-    public void batchOrderTran() {
-        
-    }
 }
