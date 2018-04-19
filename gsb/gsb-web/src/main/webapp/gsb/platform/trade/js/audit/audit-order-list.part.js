89f982f1fd4c194030b90a8bc39e9c846e87d2e7
yanxuebiao <xbyan@gongsibao.com>
2018-04-19 10:41:40 +0800

    youhua

--- gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit/audit-order-list.part.js
+++ gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit/audit-order-list.part.js
@@ -28,7 +28,7 @@ com.gongsibao.trade.web.AuditOrderListPart = org.netsharp.panda.commerce.ListPar
             fixed: false,
             maxmin: true,
             shadeClose: true,
-            area: ['50%', '70%'],
+            area: ['70%', '70%'],
             zIndex: 100000,
             id: "orderIframe",
             content: contentUrl,
