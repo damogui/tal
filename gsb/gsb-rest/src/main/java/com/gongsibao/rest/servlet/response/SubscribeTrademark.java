58d12da238492bc80bd3fdc98f7947c1a2acf46b
penghongbao <penghongbao@liangyibang.com>
2018-04-20 14:48:56 +0800

    测试扫码关注

--- gsb/gsb-rest/src/main/java/com/gongsibao/rest/servlet/response/SubscribeTrademark.java
+++ gsb/gsb-rest/src/main/java/com/gongsibao/rest/servlet/response/SubscribeTrademark.java
@@ -29,6 +29,7 @@ public class SubscribeTrademark implements IWeixinSubscriber {
 
     }
     public ResponseMessage reply(EventRequest request, Fans fans, PublicAccount publicAccount, int sceneId){
+        SubscribeEvent eventRequest = (SubscribeEvent) request;
         NewsResponse news = new NewsResponse();
         {
             news.setToUserName(request.getFromUserName());
@@ -37,7 +38,7 @@ public class SubscribeTrademark implements IWeixinSubscriber {
         Article article = new Article();
         {
             article.setTitle( "商标进度");
-            article.setDescription("查看进度");
+            article.setDescription("查看进度"+eventRequest.getEventKey());
             article.setPicUrl("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524215171441&di=898f1b96ff4caad290f171666e725985&imgtype=0&src=http%3A%2F%2Fscimg.jb51.net%2Fallimg%2F151219%2F13-15121922361b92.jpg");
             article.setUrl( "http://icompany.gongsibao.net/index.html#/mine/couponlist");
         }
