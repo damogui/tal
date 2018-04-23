4393ab4bca21c07a0632887930245f3e38cf2ace
李飞飞 <lifeifei@exinlei.com>
2018-04-23 10:38:36 +0800

    FixBug#调整订单搜索条件

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/OrderService.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/OrderService.java
@@ -454,13 +454,13 @@ public class OrderService extends PersistableService<SoOrder> implements IOrderS
         Oql oql = new Oql();
         oql.setType(this.type);
         oql.setSelects("*");
-        sql.append(" account_id = ? and change_price_audit_status_id not in (1051,1052,1053) ");
+        sql.append(" SoOrder.accountId = ? and SoOrder.changePriceAuditStatus not in (1051,1052,1053) ");
         if (status == 1) {//未付款
-            sql.append(" and so_order.pay_status_id in (3011, 3012) AND so_order.process_status_id <> 3023 ");
+            sql.append(" and SoOrder.payStatus in (3011, 3012) AND SoOrder.processStatus <> 3023 ");
         } else if (status == 2) {//办理中3022
-            sql.append(" and so_order.process_status_id = 3022 AND so_order.pay_status_id = 3013 ");
+            sql.append(" and SoOrder.processStatus = 3022 AND SoOrder.payStatus = 3013 ");
         } else if (status == 3) {//办理完成
-            sql.append(" and so_order.process_status_id = 3024 AND so_order.pay_status_id = 3013 ");
+            sql.append(" and SoOrder.processStatus = 3024 and SoOrder.payStatus = 3013 ");
         }
         oql.setFilter(sql.toString());
         oql.getParameters().add("account_id", accountId, Types.INTEGER);
@@ -483,7 +483,7 @@ public class OrderService extends PersistableService<SoOrder> implements IOrderS
             sql.append(" and SoOrder.processStatus = 3024 and SoOrder.payStatus = 3013 ");
         }
         oql.setFilter(sql.toString());
-        oql.setOrderby(" SoOrder.createTime DESC ");
+        oql.setOrderby(" createTime DESC ");
         oql.setPaging(new Paging(currentPage, pageSize));
         oql.getParameters().add("account_id", accountId, Types.INTEGER);
         return this.pm.queryList(oql);
