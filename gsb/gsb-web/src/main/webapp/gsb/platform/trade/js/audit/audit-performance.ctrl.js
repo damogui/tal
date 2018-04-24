19190434535d0d1b650073b4b1c36ed64c19cc06
gj <jjguo@gongsibao.com>
2018-04-24 10:36:49 +0800

    seo

--- gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit/audit-performance.ctrl.js
+++ gsb/gsb-web/src/main/webapp/gsb/platform/trade/js/audit/audit-performance.ctrl.js
@@ -68,7 +68,8 @@ com.gongsibao.trade.web.AuditPerformanceCtrl = com.gongsibao.trade.web.AuditBase
                     {field: 'creator', title: '审批人', width: 100, align: 'center'},
                     {field: 'option', title: '操作', width: 150},
                     {field: 'remark', title: '审批记录内容', width: 150},
-                    {field: 'createTime', title: '操作时间', width: 150}
+                    {field: 'createTime', title: '操作时间', width: 150},
+                    {field: 'remark', title: '说明', width: 150}
 
                 ]]
             });
