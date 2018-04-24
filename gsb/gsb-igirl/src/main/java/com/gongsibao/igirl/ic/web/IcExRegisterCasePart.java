e08045c873179cbe62fe40db694bdbaf5a1b0234
dong <xydong@gongsibao.com>
2018-04-24 12:45:03 +0800

    工商状态更新日志

--- gsb/gsb-igirl/src/main/java/com/gongsibao/igirl/ic/web/IcExRegisterCasePart.java
+++ gsb/gsb-igirl/src/main/java/com/gongsibao/igirl/ic/web/IcExRegisterCasePart.java
@@ -2,17 +2,19 @@ package com.gongsibao.igirl.ic.web;
 
 import com.gongsibao.entity.igirl.ic.ex.IcExRegisterCase;
 import com.gongsibao.entity.igirl.tm.TransferTradeMark;
+import com.gongsibao.igirl.ic.base.IcExRegisterService;
 import com.gongsibao.igirl.ic.service.RegisterCaseService;
+import org.netsharp.communication.ServiceFactory;
 import org.netsharp.panda.commerce.FormPart;
 
 public class IcExRegisterCasePart extends FormPart {
-    RegisterCaseService registerCaseService = new RegisterCaseService();
-
-    /*public IcExRegisterCase updateOwner(Integer ttmId, Integer ownerId){
-       return registerCaseService
-    }*/
-
-    /*public Integer updateState(String id, Integer state){
-
-    }*/
+    IcExRegisterService service = ServiceFactory.create(IcExRegisterService.class);
+    public Integer findCom(String approvalName){
+        Integer result=0;
+        IcExRegisterCase icCase= service.findCom(approvalName);
+        if(icCase!=null){
+            result = 1;
+        }
+        return result;
+    }
 }
