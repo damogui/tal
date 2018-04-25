e7f79d7abdb56bddd605157f9689958fd553bf59
yanxuebiao <xbyan@gongsibao.com>
2018-04-25 10:01:30 +0800

    优化

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/order/stage/ActionApplyStageWriteBack.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/order/stage/ActionApplyStageWriteBack.java
@@ -22,10 +22,6 @@ public class ActionApplyStageWriteBack implements IAction{
 		{
 			updateSql.update("so_order");
 			updateSql.set("installment_audit_status_id", AuditStatusType.Shz.getValue());
-			updateSql.set("stage_create_time", order.getStageCreateTime());
-			updateSql.set("installment_mode", order.getInstallmentMode());			
-			updateSql.set("stage_creator",SessionManager.getUserName());
-			updateSql.set("stage_num", order.getStages().size());
 			updateSql.where("pkid =" + order.getId());
 		}
 		String cmdText = updateSql.toSQL();
