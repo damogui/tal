a8934c172c3820a6cae4ae3867c20827aeb03f12
hanwei <hwhping@163.com>
2018-04-26 11:57:09 +0800

    优化

--- gsb/gsb-entity/src/main/java/com/gongsibao/entity/trade/dic/SginingCompanyType.java
+++ gsb/gsb-entity/src/main/java/com/gongsibao/entity/trade/dic/SginingCompanyType.java
@@ -9,8 +9,17 @@ import org.netsharp.base.IEnum;
  */
 public enum SginingCompanyType  implements IEnum{
 
-	CONSULT(3161, "汉唐信通（北京）咨询股份有限公司"), LTD(3162, "汉唐信通（北京）科技有限公司");
-	
+	SGINING_COMPANY_1(3161, "汉唐信通（北京）咨询股份有限公司"), 
+	SGINING_COMPANY_2(3162, "汉唐信通（北京）科技有限公司"),
+	SGINING_COMPANY_3(3163, "汉唐信通（北京）登记注册代理事务所"),
+	SGINING_COMPANY_4(3164, "上海诺唐商务咨询有限公司"),
+	SGINING_COMPANY_5(3165, "汉世唐源（北京）咨询有限公司"),
+	SGINING_COMPANY_6(3166, "唐源"),
+	SGINING_COMPANY_7(3167, "信通"),
+	SGINING_COMPANY_8(3168, "掌迅东方（北京）信息技术有限公司"),
+	SGINING_COMPANY_9(3169, "汉唐信通（北京）投资管理有限公司"),
+	OTHER(31610, "其他");
+
 	private int value;
 	private String text;
 
