6e57f59ce9fe3a351284c6f1b6e3383c3e51d490
penghongbao <penghongbao@liangyibang.com>
2018-04-26 17:19:51 +0800

    立即支付

--- gsb/gsb-account/src/main/java/com/gongsibao/account/service/AccountWeiXinService.java
+++ gsb/gsb-account/src/main/java/com/gongsibao/account/service/AccountWeiXinService.java
@@ -309,6 +309,7 @@ public class AccountWeiXinService extends PersistableService<AccountWeiXin> impl
                 data.getKeynotes().put("keyword3", new KeyNote(date));
                 if (StringUtils.isNotEmpty(remark)) {
                     data.getKeynotes().put("remark", new KeyNote(remark));
+                    data.setRemark(new KeyNote(remark));
                 } else {
                     data.getKeynotes().put("remark", new KeyNote(""));
                     data.setRemark(new KeyNote(""));
@@ -412,7 +413,7 @@ public class AccountWeiXinService extends PersistableService<AccountWeiXin> impl
             if (null != proName && null != orderNo) {
                 //取用户信息
                 Account account = accountService.byMobile(mobile);
-                this.pushTextMsgByOriginalId(originalId, account.getId(), "您的订单已创建成功,产品[" + proName + "]", orderNo, String.valueOf(payablePrice), addTime, "/index.html#/orderDetails/" + SecurityUtils.rc4Encrypt(pkid), "点击进行支付", AccountWxMsg.ORDER_SUCCESS);
+                this.pushTextMsgByOriginalId(originalId, account.getId(), "您的订单已创建成功,产品[" + proName + "]", orderNo, String.valueOf(payablePrice), addTime, "/index.html#/orderDetails/" + SecurityUtils.rc4Encrypt(pkid), "点击立即支付", AccountWxMsg.ORDER_SUCCESS);
             }
         } catch (SQLException e) {
             e.printStackTrace();
