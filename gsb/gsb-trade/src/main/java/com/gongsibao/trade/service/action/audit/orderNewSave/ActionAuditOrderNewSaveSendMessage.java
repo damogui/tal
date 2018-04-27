80147193fc8af62565f08b2038713c92b08bd730
zhangchao <chaozhang@gongsibao.com>
2018-04-27 13:48:34 +0800

    fix

--- gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/audit/orderNewSave/ActionAuditOrderNewSaveSendMessage.java
+++ gsb/gsb-trade/src/main/java/com/gongsibao/trade/service/action/audit/orderNewSave/ActionAuditOrderNewSaveSendMessage.java
@@ -1,5 +1,6 @@
 package com.gongsibao.trade.service.action.audit.orderNewSave;
 
+import com.gongsibao.account.base.IAccountWeiXinService;
 import com.gongsibao.bd.service.auditLog.AuditContext;
 import com.gongsibao.bd.service.auditLog.AuditState;
 import com.gongsibao.entity.bd.AuditLog;
@@ -17,92 +18,103 @@ import java.util.List;
 import java.util.Map;
 
 /*订单改价审核*/
-public class ActionAuditOrderNewSaveSendMessage implements IAction{
+public class ActionAuditOrderNewSaveSendMessage implements IAction {
 
-	@Override
-	public void execute(ActionContext ctx) {
-		AuditContext auditContext = (AuditContext) ctx.getItem();
-		Map<String, Object> objectMap = ctx.getStatus();
-		AuditLog auditLog = (AuditLog) objectMap.get("auditLog");
-		SoOrder soOrder = (SoOrder) objectMap.get("soOrder");
-		//本次审核通过或驳回
-		AuditState state = auditContext.getState();
-		//审核
-		//审核意见
-		String remark = auditContext.getremark();
-		auditSend(state, auditLog, soOrder, remark);
-	}
+    private IAccountWeiXinService accountWeiXinService = ServiceFactory.create(IAccountWeiXinService.class);
 
-	/*进行发送消息*/
-	private void auditSend(AuditState state, AuditLog auditLog, SoOrder soOrder, String remark) {
+    @Override
+    public void execute(ActionContext ctx) {
+        AuditContext auditContext = (AuditContext) ctx.getItem();
+        Map<String, Object> objectMap = ctx.getStatus();
+        AuditLog auditLog = (AuditLog) objectMap.get("auditLog");
+        SoOrder soOrder = (SoOrder) objectMap.get("soOrder");
+        //本次审核通过或驳回
+        AuditState state = auditContext.getState();
+        //审核
+        //审核意见
+        String remark = auditContext.getremark();
+        //auditSend(state, auditLog, soOrder, remark);
+        //推送icompnay公众号消息
+        sendWxMsg(state, auditLog, soOrder);
+    }
 
-		if (soOrder == null || auditLog == null) {
+    /*进行发送消息*/
+    private void auditSend(AuditState state, AuditLog auditLog, SoOrder soOrder, String remark) {
 
-			return;
-		}
-		switch (state.getValue()) {
-			case 0://驳回审核
-				if (soOrder.getOwner() != null) {
+        if (soOrder == null || auditLog == null) {
 
-					String content = String.format("【回款业绩审核提醒】您好，您有1个订单提交的回款业绩申请审核不通过，订单编号为【%S】，原因为【%s】,请知悉", soOrder.getNo(),remark);
-					SmsHelper.send(soOrder.getOwner().getMobile(), content);//订单业务员
-					sendAuditFail(soOrder.getOwner().getName(),soOrder.getId(), soOrder.getNo(), remark);//业绩相关业务员
-				}
-				break;
-			case 1://通过审核
-				if (auditLog.getLevel().equals(auditLog.getMaxLevel())) {
-					//通过审核
-					String content = String.format("【回款业绩审核提醒】您好，您有1个订单提交的回款业绩申请已审核通过，订单编号为【%s】，请知悉", soOrder.getNo());
-					SmsHelper.send(soOrder.getOwner().getMobile(), content);//订单业务员
-					sendAuditPass(soOrder.getOwner().getName(),soOrder.getId(), soOrder.getNo());//业绩相关业务员
-				} else {
-					//通知下一级
-					List<Integer> userIds = AuditHelper.getNextLevelUserIds(soOrder.getId(), auditLog.getType().getValue(), auditLog.getLevel() + 1);//获取下一级要通知的人 fromId
-					sendNextAudit(userIds, soOrder.getNo());//下一级审核
+            return;
+        }
+        switch (state.getValue()) {
+            case 0://驳回审核
+                if (soOrder.getOwner() != null) {
 
-				}
-				break;
-		}
+                    String content = String.format("【回款业绩审核提醒】您好，您有1个订单提交的回款业绩申请审核不通过，订单编号为【%S】，原因为【%s】,请知悉", soOrder.getNo(), remark);
+                    SmsHelper.send(soOrder.getOwner().getMobile(), content);//订单业务员
+                    sendAuditFail(soOrder.getOwner().getName(), soOrder.getId(), soOrder.getNo(), remark);//业绩相关业务员
+                }
+                break;
+            case 1://通过审核
+                if (auditLog.getLevel().equals(auditLog.getMaxLevel())) {
+                    //通过审核
+                    String content = String.format("【回款业绩审核提醒】您好，您有1个订单提交的回款业绩申请已审核通过，订单编号为【%s】，请知悉", soOrder.getNo());
+                    SmsHelper.send(soOrder.getOwner().getMobile(), content);//订单业务员
+                    sendAuditPass(soOrder.getOwner().getName(), soOrder.getId(), soOrder.getNo());//业绩相关业务员
+                } else {
+                    //通知下一级
+                    List<Integer> userIds = AuditHelper.getNextLevelUserIds(soOrder.getId(), auditLog.getType().getValue(), auditLog.getLevel() + 1);//获取下一级要通知的人 fromId
+                    sendNextAudit(userIds, soOrder.getNo());//下一级审核
 
-	}
+                }
+                break;
+        }
 
-	/*下一级审核*/
-	private void sendNextAudit(List<Integer> userIds, String no) {
-		for (Integer item:userIds
-				) {
-			String content = String.format("【订单业绩待审核提醒】您好，有1个订单业绩申请待您审核，请及时审核");
-			SmsHelper.send(UserHelper.getEmployeTelById(item), content);//电话和内容
-		}
+    }
 
-	}
+    /*下一级审核*/
+    private void sendNextAudit(List<Integer> userIds, String no) {
+        for (Integer item : userIds
+                ) {
+            String content = String.format("【订单业绩待审核提醒】您好，有1个订单业绩申请待您审核，请及时审核");
+            SmsHelper.send(UserHelper.getEmployeTelById(item), content);//电话和内容
+        }
 
-	/*审核通过*/
-	private void sendAuditPass(String owerName,Integer id, String no) {
+    }
 
-
-		List<NDepReceivable> ndeps = ndepService.getNDepsByOrderId(id);
-		for (NDepReceivable item : ndeps
-				) {
-			String content = String.format("【回款业绩审核提醒】您好，【%s】分配给您的回款业绩，审核已通过，订单编号为【%s】，请知悉",owerName, no);
-			SmsHelper.send(UserHelper.getEmployeTelById(item.getSalesmanId()), content);//电话和内容
-
-		}
-	}
-
-	/*审核失败*/
-	INDepReceivableService ndepService = ServiceFactory.create(INDepReceivableService.class);
-
-	private void sendAuditFail(String owerName,Integer id, String orderNo, String remark) {
-
-		List<NDepReceivable> ndeps = ndepService.getNDepsByOrderId(id);
-		for (NDepReceivable item : ndeps
-				) {
-			String content = String.format("【回款业绩审核提醒】您好，【%s】分配给您的回款业绩，审核未通过，订单编号为【%s】，原因为【%s】,请知悉", owerName,orderNo, remark);
-			SmsHelper.send(UserHelper.getEmployeTelById(item.getSalesmanId()), content);//电话和内容
-
-		}
+    /*审核通过*/
+    private void sendAuditPass(String owerName, Integer id, String no) {
 
 
-	}
+        List<NDepReceivable> ndeps = ndepService.getNDepsByOrderId(id);
+        for (NDepReceivable item : ndeps
+                ) {
+            String content = String.format("【回款业绩审核提醒】您好，【%s】分配给您的回款业绩，审核已通过，订单编号为【%s】，请知悉", owerName, no);
+            SmsHelper.send(UserHelper.getEmployeTelById(item.getSalesmanId()), content);//电话和内容
+
+        }
+    }
+
+    /*审核失败*/
+    INDepReceivableService ndepService = ServiceFactory.create(INDepReceivableService.class);
+
+    private void sendAuditFail(String owerName, Integer id, String orderNo, String remark) {
+
+        List<NDepReceivable> ndeps = ndepService.getNDepsByOrderId(id);
+        for (NDepReceivable item : ndeps
+                ) {
+            String content = String.format("【回款业绩审核提醒】您好，【%s】分配给您的回款业绩，审核未通过，订单编号为【%s】，原因为【%s】,请知悉", owerName, orderNo, remark);
+            SmsHelper.send(UserHelper.getEmployeTelById(item.getSalesmanId()), content);//电话和内容
+
+        }
+
+
+    }
+
+    private void sendWxMsg(AuditState state, AuditLog auditLog, SoOrder soOrder) {
+        if (state.equals(AuditState.PASS) && auditLog.getLevel().equals(auditLog.getMaxLevel())) {
+            //推送icompany公众号的模板消息
+            accountWeiXinService.saveOrderMsg(soOrder.getAccountMobile(), soOrder.getId());
+        }
+    }
 
 }
