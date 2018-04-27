03230bf3b1f95c8cec9ef15569303a57d7839d9b
penghongbao <penghongbao@liangyibang.com>
2018-04-27 11:04:09 +0800

    消息发送oauth

--- gsb/gsb-account/src/main/java/com/gongsibao/account/base/IAccountWeiXinService.java
+++ gsb/gsb-account/src/main/java/com/gongsibao/account/base/IAccountWeiXinService.java
@@ -47,4 +47,19 @@ public interface IAccountWeiXinService extends IPersistableService<AccountWeiXin
 	 * @date 2018/4/27 10:15
 	 */
 	void sendLinkMsg(String title,String content,String url,int accountId);
+	/**
+	 * @Description:TODO Oauth 方式必须与公众号下域名下
+	 * @param
+	 * @return
+	 * @author hbpeng <hbpeng@gongsibao.com>
+	 * @date 2018/4/27 11:03
+	 */
+	void pushTextMsgOauth(Integer accountId,
+						  String first,
+						  String keyword1,
+						  String keyword2,
+						  String date,
+						  String url,
+						  String remark,
+						  AccountWxMsg tmpId);
 }
