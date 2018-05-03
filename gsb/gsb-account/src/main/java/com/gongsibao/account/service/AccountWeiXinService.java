2acf0e3f304673696850ade324a0007cce176706
penghongbao <penghongbao@liangyibang.com>
2018-05-03 16:02:14 +0800

    精度修改

--- gsb/gsb-account/src/main/java/com/gongsibao/account/service/AccountWeiXinService.java
+++ gsb/gsb-account/src/main/java/com/gongsibao/account/service/AccountWeiXinService.java
@@ -481,7 +481,8 @@ public class AccountWeiXinService extends PersistableService<AccountWeiXin> impl
             if (null != proName && null != orderNo) {
                 //取用户信息
                 Account account = accountService.byMobile(mobile);
-                this.pushTextMsgByOriginalId(originalId, account.getId(), "您的订单已创建成功,产品[" + proName + "]", orderNo, String.valueOf(AmountUtils.div(payablePrice,100)), addTime, "/index.html#/orderDetails/" + SecurityUtils.rc4Encrypt(pkid), "点击立即支付", AccountWxMsg.ORDER_SUCCESS);
+                DecimalFormat    df   = new DecimalFormat("#######.00");
+                this.pushTextMsgByOriginalId(originalId, account.getId(), "您的订单已创建成功,产品[" + proName + "]", orderNo, df.format(Double.valueOf(payablePrice)/100), addTime, "/index.html#/orderDetails/" + SecurityUtils.rc4Encrypt(pkid), "点击立即支付", AccountWxMsg.ORDER_SUCCESS);
             }
         } catch (SQLException e) {
             e.printStackTrace();
